55a0fe1fc4baecebaf02bb6f0ca3354376863f08
sxydh <815908561@qq.com>
2022-11-11 15:36:12 +0800

    [ADD][h5-frs-server]

---
+++ h5-frs/h5-frs-server/src/main/java/cn/net/bhe/h5frsserver/H5FrsServerApplication.java
@@ -0,0 +1,13 @@
+package cn.net.bhe.h5frsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H5FrsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H5FrsServerApplication.class, args);
+	}
+
+}
---
+++ h5-frs/h5-frs-server/src/test/java/cn/net/bhe/h5frsserver/H5FrsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.net.bhe.h5frsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H5FrsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
