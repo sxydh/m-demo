ea7a14be40decd75924292b18f951585382d4a8b
sxydh <815908561@qq.com>
2022-11-21 23:25:05 +0800

    [UPDATE][activiti-springboot]rebuild

--- activiti-springboot/README.md
+++ activiti-springboot/README.md
@@ -5,40 +5,13 @@ SpringBoot2.7.2集成Activiti6.0.0
 ## Quick Start
 
 * 配置数据库  
-  activiti.cfg.xml  
-
-  ```xml
-  <property name="databaseType" value="oracle"></property>
-  <property name="jdbcUrl" value="jdbc:oracle:thin:@127.0.0.1:1521/orcl"></property>
-  <property name="jdbcDriver" value="oracle.jdbc.driver.OracleDriver"></property>
-  <property name="jdbcUsername" value="bhe"></property>
-  <property name="jdbcPassword" value="123"></property>
-  <property name="databaseSchema" value="BHE"></property>
-  <property name="databaseSchemaUpdate" value="true"></property>
-  ```
-
   application.properties  
 
   ```Properties
-  spring.datasource.druid.url=jdbc:oracle:thin:@127.0.0.1:1521/orcl
-  spring.datasource.druid.username=bhe
-  spring.datasource.druid.password=123
-  spring.datasource.druid.driver-class-name=oracle.jdbc.OracleDriver
-  ```
-
-* 初始化Activiti表  
-  ProcessEngineBuildApplication.java
-
-  ```java
-  public class ProcessEngineBuildApplication {
-  
-      public static void main(String[] args) {
-          ProcessEngineConfiguration configuration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
-          ProcessEngine processEngine = configuration.buildProcessEngine();
-          System.out.println(processEngine);
-      }
-  
-  }
+  spring.datasource.druid.url=jdbc:mysql://localhost:3306/actboot?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&  serverTimezone=GMT%2B8&nullCatalogMeansCurrent=true
+  spring.datasource.druid.username=root
+  spring.datasource.druid.password=123456
+  spring.datasource.druid.driver-class-name=com.mysql.cj.jdbc.Driver
   ```
 
 * 启动项目  
@@ -56,3 +29,8 @@ SpringBoot2.7.2集成Activiti6.0.0
   
   }
   ```
+
+  > **Note**  
+  >
+  > 首次运行时Activiti表会自动初始化  
+  > resources/processes下的流程文件默认自动部署
--- activiti-springboot/src/main/java/cn/net/bhe/activitispringboot/controller/ActivitiController.java
+++ activiti-springboot/src/main/java/cn/net/bhe/activitispringboot/controller/ActivitiController.java
@@ -4,17 +4,18 @@ import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.impl.identity.Authentication;
+import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
-import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Administrator
@@ -31,50 +32,42 @@ public class ActivitiController {
     private TaskService taskService;
 
     @RequestMapping("/deploy")
-    public void deploy() {
-        String resourceName = "processes/diagram.bpmn";
-        repositoryService.createDeployment()
-                .addClasspathResource(resourceName)
+    public String deploy(@RequestParam("bpmn") MultipartFile bpmn) throws Exception {
+        Deployment deployment = repositoryService.createDeployment()
+                // 任意取
+                .name(bpmn.getOriginalFilename())
+                // resourceName后缀必须是bpmn
+                .addInputStream(
+                        Objects.requireNonNull(bpmn.getOriginalFilename())
+                                .substring(0, bpmn.getOriginalFilename().indexOf(".")) + ".bpmn",
+                        bpmn.getInputStream())
                 .deploy();
+        System.out.println(deployment);
+        return deployment.getId();
     }
 
     @RequestMapping("/start")
-    public void start() {
+    public String start(@RequestParam String processDefinitionKey) {
+        // setAuthenticatedUserId设置流程实例的start_user_id_
         Authentication.setAuthenticatedUserId(RandomStringUtils.randomAlphabetic(5));
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(
-                "Process_1",
+                // 流程key
+                processDefinitionKey,
+                // 业务唯一标识
                 RandomStringUtils.randomAlphabetic(10),
+                // 流程变量
                 Map.of(
                         "key1", RandomStringUtils.randomAlphabetic(3),
                         "key2", RandomStringUtils.randomAlphabetic(3)));
         System.out.println(processInstance);
-        claim(taskListDo());
-    }
-
-    private void claim(List<Task> taskList) {
-        for (Task ele : taskList) {
-            if (ObjectUtils.isEmpty(ele.getAssignee())) {
-                taskService.claim(ele.getId(), RandomStringUtils.randomAlphabetic(5));
-            }
-        }
-    }
-
-    @RequestMapping("/taskList")
-    public void taskList() {
-        taskListDo();
-    }
-
-    private List<Task> taskListDo() {
-        List<Task> taskList = taskService.createTaskQuery().orderByTaskCreateTime().asc().list();
-        System.out.println(taskList);
-        return taskList;
+        return processInstance.getId();
     }
 
     @RequestMapping("/complete")
-    public void complete() {
-        List<Task> taskList = taskListDo();
-        Assert.notEmpty(taskList, "流程已结束");
-        Task task = taskList.get(0);
+    public String complete() {
+        // 执行最近的任务
+        Task task = taskService.createTaskQuery().orderByTaskCreateTime().desc().list().get(0);
+        // 设置执行人
         taskService.setAssignee(task.getId(), RandomStringUtils.randomAlphabetic(5));
         taskService.complete(
                 task.getId(),
@@ -82,9 +75,7 @@ public class ActivitiController {
                         "gatewayVal", "1",
                         "key1", RandomStringUtils.randomAlphabetic(3),
                         "key2", RandomStringUtils.randomAlphabetic(3)));
-        taskList = taskListDo();
-        Assert.notEmpty(taskList, "流程已结束");
-        claim(taskList);
+        return task.getId();
     }
 
 }
