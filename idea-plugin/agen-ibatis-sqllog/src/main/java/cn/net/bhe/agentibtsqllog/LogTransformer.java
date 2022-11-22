5481b5be8cf2cb338e9b38b07d0b38c7a415d4fd
sxydh <815908561@qq.com>
2022-11-22 15:30:44 +0800

    [UPDATE][agen-ibatis-sqllog]rebuild

--- idea-plugin/agen-ibatis-sqllog/src/main/java/cn/net/bhe/agentibtsqllog/LogTransformer.java
+++ idea-plugin/agen-ibatis-sqllog/src/main/java/cn/net/bhe/agentibtsqllog/LogTransformer.java
@@ -18,6 +18,12 @@ public class LogTransformer implements ClassFileTransformer {
     static final String RED_BOLD = "\033[1;31m";
     static final String RESET = "\033[0m";
 
+    private CtClass getCtClass(String className) {
+        ClassPool classPool = ClassPool.getDefault();
+        classPool.appendClassPath(new LoaderClassPath(Thread.currentThread().getContextClassLoader()));
+        return classPool.getOrNull(className);
+    }
+
     @Override
     public byte[] transform(ClassLoader loader,
                             String className,
@@ -27,11 +33,14 @@ public class LogTransformer implements ClassFileTransformer {
         try {
             className = className.replace("/", ".");
             String preparedStatementHandler = "org.apache.ibatis.executor.statement.PreparedStatementHandler";
-            String preparedStatementProxyImpl = "com.alibaba.druid.proxy.jdbc.PreparedStatementProxyImpl";
-            if (className.equals(preparedStatementHandler)) {
+            List<String> mysqls = Arrays.asList("com.mysql.jdbc.PreparedStatement", "com.mysql.cj.jdbc.ClientPreparedStatement");
+            List<String> oracles = Arrays.asList("", "oracle.jdbc.driver.OraclePreparedStatement");
+            if (preparedStatementHandler.equals(className)) {
                 return preparedStatementHandler(getCtClass(className));
-            } else if (className.equals(preparedStatementProxyImpl)) {
-                return preparedStatementProxyImpl(getCtClass(className));
+            } else if (mysqls.contains(className)) {
+                return clientPreparedStatement(getCtClass(className));
+            } else if (oracles.contains(className)) {
+                return oraclePreparedStatement(getCtClass(className));
             }
         } catch (Throwable e) {
             e.printStackTrace();
@@ -39,34 +48,27 @@ public class LogTransformer implements ClassFileTransformer {
         return null;
     }
 
-    private CtClass getCtClass(String className) {
-        ClassPool classPool = ClassPool.getDefault();
-        classPool.appendClassPath(new LoaderClassPath(Thread.currentThread().getContextClassLoader()));
-        return classPool.getOrNull(className);
-    }
-
     private byte[] preparedStatementHandler(CtClass ctClass) throws Exception {
         if (ctClass != null) {
             System.out.println(RED_BOLD + "agent-ibatis-sqllog [PreparedStatementHandler] is being loaded...");
             System.out.print(RESET);
-            CtField ctField = new CtField(getCtClass("java.lang.ThreadLocal"), "MS_THREAD_LOCAL", ctClass);
-            ctField.setModifiers(Modifier.PUBLIC | Modifier.STATIC | Modifier.FINAL);
-            ctClass.addField(ctField, CtField.Initializer.byNew(getCtClass("java.lang.ThreadLocal")));
+            CtField ctField = CtField.make(" public static final java.lang.ThreadLocal MS_THREAD_LOCAL = new java.lang.ThreadLocal(); ", ctClass);
+            ctClass.addField(ctField);
             for (CtConstructor ctConstructor : ctClass.getConstructors()) {
                 String name = ctConstructor.getName();
                 String targetName = "PreparedStatementHandler";
                 if (name.equals(targetName)) {
                     ctConstructor.insertAfter("" +
-                            "        try {\n" +
-                            "            MS_THREAD_LOCAL.set($2.getId());\n" +
-                            "        } catch (Throwable e) {\n" +
-                            "            e.printStackTrace();\n" +
-                            "        };\n" +
+                            "try {\n" +
+                            "    MS_THREAD_LOCAL.set($2.getId());\n" +
+                            "} catch (Throwable e) {\n" +
+                            "    e.printStackTrace();\n" +
+                            "};\n" +
                             "");
                     break;
                 }
             }
-            ctClass.writeFile("C:\\Users\\Administrator\\Desktop");
+            /* ctClass.writeFile("C:\\Users\\Administrator\\Desktop"); */
             System.out.println(RED_BOLD + "agent-ibatis-sqllog [PreparedStatementHandler] has been loaded.");
             System.out.print(RESET);
             return ctClass.toBytecode();
@@ -74,58 +76,137 @@ public class LogTransformer implements ClassFileTransformer {
         return null;
     }
 
-    private byte[] preparedStatementProxyImpl(CtClass ctClass) throws Exception {
+    private void addFieldParamsMap(CtClass ctClass) throws Exception {
+        CtField ctField = CtField.make(" private final java.util.Map paramsMap = new java.util.HashMap(); ", ctClass);
+        ctClass.addField(ctField);
+    }
+
+    private void interceptMethodSetAny(CtMethod method) throws Exception {
+        List<String> setMethods1 = Arrays.asList("setNull", "setBoolean", "setShort", "setInt", "setFloat", "setDouble", "setLong", "setBigDecimal");
+        List<String> setMethods2 = Arrays.asList("setString", "setDate", "setTime", "setTimestamp", "setArray", "setObject");
+        if (setMethods1.contains(method.getName())) {
+            method.insertBefore("" +
+                    "try {\n" +
+                    "    paramsMap.put(Integer.valueOf($1), String.valueOf($2));\n" +
+                    "} catch (Throwable e) {\n" +
+                    "    e.printStackTrace();\n" +
+                    "}\n" +
+                    "");
+        } else if (setMethods2.contains(method.getName())) {
+            method.insertBefore("" +
+                    "try {\n" +
+                    "    paramsMap.put(Integer.valueOf($1), \"'\" + $2 + \"'\");\n" +
+                    "} catch (Throwable e) {\n" +
+                    "    e.printStackTrace();\n" +
+                    "}\n" +
+                    "");
+        }
+    }
+
+    private byte[] clientPreparedStatement(CtClass ctClass) throws Exception {
         if (ctClass != null) {
-            System.out.println(RED_BOLD + "agent-ibatis-sqllog [PreparedStatementProxyImpl] is being loaded...");
+            System.out.println(RED_BOLD + "agent-ibatis-sqllog [ClientPreparedStatement] is being loaded...");
             System.out.print(RESET);
+            addFieldParamsMap(ctClass);
             for (CtMethod ctMethod : ctClass.getDeclaredMethods()) {
-                String ctMethodName = ctMethod.getName();
-                List<String> targetMethodNames = Arrays.asList("execute", "executeQuery", "executeUpdate");
-                if (targetMethodNames.contains(ctMethodName)) {
+                interceptMethodSetAny(ctMethod);
+                if ("executeInternal".equals(ctMethod.getName())) {
                     ctMethod.insertBefore("" +
-                            "        try {\n" +
-                            "            String BLACK_BOLD = \"\033[1;30m\";\n" +
-                            "            String RED_BOLD = \"\033[1;31m\";\n" +
-                            "            String GREEN_BOLD = \"\033[1;32m\";\n" +
-                            "            String YELLOW_BOLD = \"\033[1;33m\";\n" +
-                            "            String BLUE_BOLD = \"\033[1;34m\";\n" +
-                            "            String PURPLE_BOLD = \"\033[1;35m\";\n" +
-                            "            String CYAN_BOLD = \"\033[1;36m\";\n" +
-                            "            String WHITE_BOLD = \"\033[1;37m\";\n" +
-                            "            String RESET = \"\033[0m\";\n" +
-                            "            String id = null;\n" +
-                            "            try {\n" +
-                            "                java.lang.reflect.Field field = org.apache.ibatis.executor.statement.PreparedStatementHandler.class.getField(\"MS_THREAD_LOCAL\");\n" +
-                            "                ThreadLocal threadLocal = (ThreadLocal) field.get(null);\n" +
-                            "                id = (String) threadLocal.get();\n" +
-                            "                threadLocal.remove();\n" +
-                            "            } catch (Throwable e) {\n" +
-                            "            }" +
-                            "            String sql = getSql();\n" +
-                            "            java.util.Map jdbcParameterMap = getParameters();\n" +
-                            "            if (jdbcParameterMap.size() > 0) {\n" +
-                            "                Object[] paramObjs = jdbcParameterMap.values().toArray();\n" +
-                            "                int i = 0;\n" +
-                            "                while (sql.contains(\"?\")) {\n" +
-                            "                    com.alibaba.druid.proxy.jdbc.JdbcParameter jdbcParameter = (com.alibaba.druid.proxy.jdbc.JdbcParameter) paramObjs[i++];\n" +
-                            "                    Object value = jdbcParameter.getValue();\n" +
-                            "                    value = \"'\" + value + \"'\";\n" +
-                            "                    sql = sql.replaceFirst(\"\\\\?\", String.valueOf(value));\n" +
-                            "                }\n" +
-                            "            }\n" +
-                            "            System.out.println(RESET);\n" +
-                            "            System.out.println(RED_BOLD + id + \" ==> \");\n" +
-                            "            System.out.println(CYAN_BOLD + sql);\n" +
-                            "            System.out.println(RESET);\n" +
-                            "        } catch (Throwable e) {\n" +
-                            "            e.printStackTrace();\n" +
-                            "        }\n" +
+                            "try {\n" +
+                            "    String BLACK_BOLD = \"\033[1;30m\";\n" +
+                            "    String RED_BOLD = \"\033[1;31m\";\n" +
+                            "    String GREEN_BOLD = \"\033[1;32m\";\n" +
+                            "    String YELLOW_BOLD = \"\033[1;33m\";\n" +
+                            "    String BLUE_BOLD = \"\033[1;34m\";\n" +
+                            "    String PURPLE_BOLD = \"\033[1;35m\";\n" +
+                            "    String CYAN_BOLD = \"\033[1;36m\";\n" +
+                            "    String WHITE_BOLD = \"\033[1;37m\";\n" +
+                            "    String RESET = \"\033[0m\";\n" +
+                            "    String id = null;\n" +
+                            "    try {\n" +
+                            "        java.lang.reflect.Field field = org.apache.ibatis.executor.statement.PreparedStatementHandler.class.getField(\"MS_THREAD_LOCAL\");\n" +
+                            "        ThreadLocal threadLocal = (ThreadLocal) field.get(null);\n" +
+                            "        id = (String) threadLocal.get();\n" +
+                            "        threadLocal.remove();\n" +
+                            "    } catch (Throwable e) {\n" +
+                            "    }\n" +
+                            "    if (id == null) {\n" +
+                            "        id = \"\";\n" +
+                            "    }\n" +
+                            "    java.lang.String sql = getPreparedSql();\n" +
+                            "    sql = sql.replaceAll(\"(?m)^[ \\t]*\\r?\\n\", \"\");\n" +
+                            "    sql = sql.replace(\"?\", \"#1#\");\n" +
+                            "    int i = 1;\n" +
+                            "    while (sql.contains(\"#1#\")) {\n" +
+                            "        sql = sql.replaceFirst(\"#1#\", String.valueOf(paramsMap.get(Integer.valueOf(i++))));\n" +
+                            "    }\n" +
+                            "    System.out.println(RESET);\n" +
+                            "    System.out.println(RED_BOLD + id + \" ==> \");\n" +
+                            "    System.out.println(CYAN_BOLD + sql);\n" +
+                            "    System.out.println(RESET);\n" +
+                            "} catch (Throwable e) {\n" +
+                            "    e.printStackTrace();\n" +
+                            "}\n" +
                             "");
-                    break;
                 }
             }
             ctClass.writeFile("C:\\Users\\Administrator\\Desktop");
-            System.out.println(RED_BOLD + "agent-ibatis-sqllog [PreparedStatementProxyImpl] has been loaded.");
+            System.out.println(RED_BOLD + "agent-ibatis-sqllog [ClientPreparedStatement] has been loaded.");
+            System.out.print(RESET);
+            return ctClass.toBytecode();
+        }
+        return null;
+    }
+
+    private byte[] oraclePreparedStatement(CtClass ctClass) throws Exception {
+        if (ctClass != null) {
+            System.out.println(RED_BOLD + "agent-ibatis-sqllog [OraclePreparedStatement] is being loaded...");
+            System.out.print(RESET);
+            addFieldParamsMap(ctClass);
+            for (CtMethod ctMethod : ctClass.getDeclaredMethods()) {
+                interceptMethodSetAny(ctMethod);
+                if ("executeInternal".equals(ctMethod.getName())) {
+                    ctMethod.insertBefore("" +
+                            "try {\n" +
+                            "    String BLACK_BOLD = \"\033[1;30m\";\n" +
+                            "    String RED_BOLD = \"\033[1;31m\";\n" +
+                            "    String GREEN_BOLD = \"\033[1;32m\";\n" +
+                            "    String YELLOW_BOLD = \"\033[1;33m\";\n" +
+                            "    String BLUE_BOLD = \"\033[1;34m\";\n" +
+                            "    String PURPLE_BOLD = \"\033[1;35m\";\n" +
+                            "    String CYAN_BOLD = \"\033[1;36m\";\n" +
+                            "    String WHITE_BOLD = \"\033[1;37m\";\n" +
+                            "    String RESET = \"\033[0m\";\n" +
+                            "    String id = null;\n" +
+                            "    try {\n" +
+                            "        java.lang.reflect.Field field = org.apache.ibatis.executor.statement.PreparedStatementHandler.class.getField(\"MS_THREAD_LOCAL\");\n" +
+                            "        ThreadLocal threadLocal = (ThreadLocal) field.get(null);\n" +
+                            "        id = (String) threadLocal.get();\n" +
+                            "        threadLocal.remove();\n" +
+                            "    } catch (Throwable e) {\n" +
+                            "    }\n" +
+                            "    if (id == null) {\n" +
+                            "        id = \"\";\n" +
+                            "    }\n" +
+                            "    java.lang.String sql = getOriginalSql();\n" +
+                            "    sql = sql.replaceAll(\"(?m)^[ \\t]*\\r?\\n\", \"\");\n" +
+                            "    sql = sql.replace(\"?\", \"#1#\");\n" +
+                            "    int i = 1;\n" +
+                            "    while (sql.contains(\"#1#\")) {\n" +
+                            "        sql = sql.replaceFirst(\"#1#\", String.valueOf(paramsMap.get(Integer.valueOf(i++))));\n" +
+                            "    }\n" +
+                            "    System.out.println(RESET);\n" +
+                            "    System.out.println(RED_BOLD + id + \" ==> \");\n" +
+                            "    System.out.println(CYAN_BOLD + sql);\n" +
+                            "    System.out.println(RESET);\n" +
+                            "} catch (Throwable e) {\n" +
+                            "    e.printStackTrace();\n" +
+                            "}\n" +
+                            "");
+                }
+            }
+            ctClass.writeFile("C:\\Users\\Administrator\\Desktop");
+            System.out.println(RED_BOLD + "agent-ibatis-sqllog [OraclePreparedStatement] has been loaded.");
             System.out.print(RESET);
             return ctClass.toBytecode();
         }
