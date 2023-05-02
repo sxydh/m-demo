59664f502309e28786d53299370df30361dbf730
sxydh <815908561@qq.com>
2023-05-02 09:59:48 +0800

    [ADD]redis-springboot

---
+++ redis-springboot/src/test/java/cn/net/bhe/redisspringboot/RedisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.net.bhe.redisspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
