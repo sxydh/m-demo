1792d1bda1380f5d80b4aaacde8d144fea8b0d85
sxydh <sxydhgg@gmail.com>
2023-12-03 17:11:53 +0800

    [ADD]zookeeper-demo

---
+++ zookeeper-demo/src/main/java/cn/net/bhe/zookeeperdemo/_Main.java
@@ -0,0 +1,14 @@
+package cn.net.bhe.zookeeperdemo;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.Arrays;
+
+@Log4j2
+public class _Main {
+
+    public static void main(String[] args) {
+        log.info(Arrays.toString(args));
+    }
+
+}
