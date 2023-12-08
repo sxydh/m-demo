840ff660b541f706e17a90ab434ca0c28f815ee1
sxydh <sxydhgg@gmail.com>
2023-12-08 15:04:43 +0800

    [UPDATE]python-demo/pyecharts-demo/src/main/line_demo.py

--- python-demo/pyecharts-demo/src/main/line_demo.py
+++ python-demo/pyecharts-demo/src/main/line_demo.py
@@ -1,7 +1,13 @@
+from random import randint
+
 from pyecharts.charts import Line
+from pyecharts.options import LineItem
 
 if __name__ == '__main__':
     line = Line()
     line.add_xaxis(['一月', '二月', '三月'])
-    line.add_yaxis('消费', [12321, 11232, 12321])
-    line.render()
+    line.add_yaxis('消费', [
+        LineItem(name='一月', value=randint(100, 160)),
+        LineItem(name='二月', value=randint(100, 160)),
+        LineItem(name='三月', value=randint(100, 160))])
+    line.render('line_demo.html')
