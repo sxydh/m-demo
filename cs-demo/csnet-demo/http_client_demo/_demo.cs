86a703c0b92d256ea308a6c4e3b160c8e7d8f0a2
sxydh <sxydhgg@gmail.com>
2023-12-15 18:15:40 +0800

    [ADD]cs-demo/csnet-demo/http_client_demo

---
+++ cs-demo/csnet-demo/http_client_demo/_demo.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System;
+
+namespace HttpClientDemo
+{
+    internal class Demo
+    {
+        static void Init()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = null;
+            try
+            {
+                client.BaseAddress = new Uri("http://192.168.174.1:8080");
+                response = client.GetAsync("/").Result;
+                Console.WriteLine($"服务器响应：{response.Content.ReadAsStringAsync().Result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                client.Dispose();
+                response?.Dispose();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Init();
+            Console.ReadKey();
+        }
+    }
+}
