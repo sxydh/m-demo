84411f4e85bae63b45458f2170c347938783a7a9
sxydh <sxydhgg@gmail.com>
2023-12-15 10:58:48 +0800

    [UPDATE]cs-demo/csnet-demo/socket_server_demo

--- cs-demo/csnet-demo/socket_server_demo/_demo.cs
+++ cs-demo/csnet-demo/socket_server_demo/_demo.cs
@@ -8,6 +8,7 @@ namespace SocketServerDemo
 {
     internal class Demo
     {
+
         void InitSocketServer()
         {
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -15,15 +16,14 @@ namespace SocketServerDemo
             socket.Listen(10);
             Console.WriteLine("[" + Thread.CurrentThread.ManagedThreadId + "] Server is running...");
 
+            bool pool = ThreadPool.SetMaxThreads(10, 10);
+
             while (true)
             {
                 // 阻塞接收客户端连接
-                socket.AcceptAsync().ContinueWith((task) =>
-                {
-                    Socket conn = task.Result;
-                    Console.WriteLine("Client connected: " + conn.RemoteEndPoint.ToString());
-                    ConnHandler(conn);
-                });
+                Socket conn = socket.Accept();
+                Console.WriteLine("Client connected: " + conn.RemoteEndPoint.ToString());
+                ThreadPool.QueueUserWorkItem(o => ConnHandler(conn));
             }
         }
 
