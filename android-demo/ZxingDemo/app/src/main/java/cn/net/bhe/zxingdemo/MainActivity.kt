ad8d503bdf261add930a7cb3f11079eda9c8d970
sxydh <sxydhgg@gmail.com>
2024-01-19 18:20:26 +0800

    [ADD]android-demo/ZxingDemo

---
+++ android-demo/ZxingDemo/app/src/main/java/cn/net/bhe/zxingdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.net.bhe.zxingdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
