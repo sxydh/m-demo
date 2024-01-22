b86396af0bd9d0ba70c4995c5cb6cbc562b3a0fc
sxydh <sxydhgg@gmail.com>
2024-01-22 19:41:42 +0800

    [UPDATE]spark-demo/sparkhive-demo

--- spark-demo/sparkhive-demo/src/main/scala/cn/net/bhe/sparkhivedemo/_Main.scala
+++ spark-demo/sparkhive-demo/src/main/scala/cn/net/bhe/sparkhivedemo/_Main.scala
@@ -19,15 +19,9 @@ object _Main {
 
     // 数据处理
     df.createOrReplaceTempView("log")
-    val databases: Array[String] = spark.sql(" show databases ").collect().map(_.getString(0))
-    if (!databases.contains("sh_demo")) {
-      spark.sql(" create database sh_demo ")
-    }
     spark.sql(" use sh_demo ")
-    // 表存在时日志会输出异常
-    // 日志配置已经调整为 log4j.rootLogger=WARN,logfile
     spark.sql(" create table if not exists t_log(id varchar(255), ip varchar(255), op varchar(255), value varchar(255)) ")
-    for (it <- 1 to 100) {
+    for (it <- 1 to 10) {
       spark.sql(" insert into t_log select * from log ")
     }
     spark.sql(" select count(1) from t_log ").show()
