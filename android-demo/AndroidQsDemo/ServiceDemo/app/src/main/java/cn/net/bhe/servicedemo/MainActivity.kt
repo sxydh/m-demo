b09bad3b0ecb871db084075ef3a84f53369bc0b1
sxydh <sxydhgg@gmail.com>
2024-02-01 21:11:00 +0800

    [ADD]android-demo/AndroidQsDemo/ServiceDemo

---
+++ android-demo/AndroidQsDemo/ServiceDemo/app/src/main/java/cn/net/bhe/servicedemo/MainActivity.kt
@@ -0,0 +1,20 @@
+package cn.net.bhe.servicedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cn.net.bhe.servicedemo.ui.theme.ServiceDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
