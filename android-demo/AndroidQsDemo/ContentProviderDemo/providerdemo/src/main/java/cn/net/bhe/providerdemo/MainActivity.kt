bb50ab90b5b7a3dfbd2e2056297a84a68f24403d
sxydh <sxydhgg@gmail.com>
2024-02-03 20:48:28 +0800

    [ADD]android-demo/AndroidQsDemo/ContentProviderDemo

---
+++ android-demo/AndroidQsDemo/ContentProviderDemo/providerdemo/src/main/java/cn/net/bhe/providerdemo/MainActivity.kt
@@ -0,0 +1,10 @@
+package cn.net.bhe.providerdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ android-demo/AndroidQsDemo/ContentProviderDemo/resolverdemo/src/main/java/cn/net/bhe/resolverdemo/MainActivity.kt
@@ -0,0 +1,10 @@
+package cn.net.bhe.resolverdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
