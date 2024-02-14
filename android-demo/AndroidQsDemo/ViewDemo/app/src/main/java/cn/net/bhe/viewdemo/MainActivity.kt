ea8d62766e678deeb392926163f848a7aa57adfc
sxydh <sxydhgg@gmail.com>
2024-02-14 11:07:55 +0800

    [ADD]android-demo/AndroidQsDemo/ViewDemo

---
+++ android-demo/AndroidQsDemo/ViewDemo/app/src/main/java/cn/net/bhe/viewdemo/MainActivity.kt
@@ -0,0 +1,10 @@
+package cn.net.bhe.viewdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
