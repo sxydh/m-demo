f9228a5df0619ba1d104f52845fba7465b42b9f8
sxydh <sxydhgg@gmail.com>
2024-03-25 22:45:32 +0800

    [UPDATE]

---
+++ spring-demo/springcloud-demo/integrated-gateway/springcloudgateway/src/main/java/cn/net/bhe/springcloudgateway/_Main.java
@@ -0,0 +1,16 @@
+package cn.net.bhe.springcloudgateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class _Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(_Main.class);
+    }
+
+}
