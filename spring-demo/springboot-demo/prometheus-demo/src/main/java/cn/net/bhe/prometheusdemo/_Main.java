f6252064fbdbf1652fcbf6ea8359a0a6278cddea
sxydh <sxydhgg@gmail.com>
2024-04-21 11:16:36 +0800

    [ADD]spring-demo/springboot-demo/prometheus-demo

---
+++ spring-demo/springboot-demo/prometheus-demo/src/main/java/cn/net/bhe/prometheusdemo/_Main.java
@@ -0,0 +1,13 @@
+package cn.net.bhe.prometheusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class _Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(_Main.class);
+    }
+
+}
