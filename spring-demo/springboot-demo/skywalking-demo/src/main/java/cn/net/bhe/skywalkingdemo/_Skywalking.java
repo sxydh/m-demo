b0d769979b199b71012fbf13c9547cc4106596d8
sxydh <sxydhgg@gmail.com>
2024-05-26 21:19:00 +0800

    [ADD]spring-demo/springboot-demo/skywalking-demo

---
+++ spring-demo/springboot-demo/skywalking-demo/src/main/java/cn/net/bhe/skywalkingdemo/_Skywalking.java
@@ -0,0 +1,56 @@
+package cn.net.bhe.skywalkingdemo;
+
+import cn.net.bhe.mutil.DbUtils;
+import cn.net.bhe.mutil.HttpUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+@SpringBootApplication
+@Slf4j
+@RestController
+public class _Skywalking {
+
+    public static void main(String[] args) {
+        SpringApplication.run(_Skywalking.class);
+    }
+
+    private final DbUtils dbUtils = DbUtils.build("jdbc:mysql://192.168.233.129:3306/sw_demo", "root", "123");
+
+    public _Skywalking() throws SQLException {
+    }
+
+    @GetMapping("/")
+    public String get() throws Exception {
+        List<HashMap<String, Object>> list = dbUtils.executeQuery("select uuid() as uuid");
+        return list.toString();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        Thread thread = new Thread(() -> {
+            try {
+                Random random = new Random();
+                while (true) {
+                    String getStr = HttpUtils.get("http://localhost:20010/");
+                    System.out.println(getStr);
+                    // noinspection BusyWait
+                    Thread.sleep(random.nextInt(500));
+                }
+            } catch (Exception e) {
+                log.error(e.getLocalizedMessage(), e);
+            }
+        });
+        thread.start();
+    }
+
+}
