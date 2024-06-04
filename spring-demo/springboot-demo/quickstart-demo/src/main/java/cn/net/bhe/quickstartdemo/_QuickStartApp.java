abf800afc557e0c3fc288974fd0e9c7393637225
sxydh <sxydhgg@gmail.com>
2024-06-04 16:09:04 +0800

    [UPDATE]

--- spring-demo/springboot-demo/quickstart-demo/src/main/java/cn/net/bhe/quickstartdemo/_QuickStartApp.java
+++ spring-demo/springboot-demo/quickstart-demo/src/main/java/cn/net/bhe/quickstartdemo/_QuickStartApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class _Main {
+public class _QuickStartApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(_Main.class);
+        SpringApplication.run(_QuickStartApp.class);
     }
 
 }
