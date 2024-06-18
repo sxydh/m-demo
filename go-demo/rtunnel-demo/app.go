2a21ff0e8397424431d99b703da1875ea7452171
sxydh <sxydhgg@gmail.com>
2024-06-19 01:39:46 +0800

    [UPDATE]

--- go-demo/rtunnel-demo/app.go
+++ go-demo/rtunnel-demo/app.go
@@ -19,8 +19,8 @@ import (
 
 func main() {
 	ssh_utils.NewReverseTunnel(&[]*ssh_utils.Tunnel{
-		{SSHIp: "124.71.35.157", SSHPort: 22, SSHUser: "root", ListenPort: 40010, TargetIp: "localhost", TargetPort: 40010},
-		{SSHIp: "124.71.35.157", SSHPort: 22, SSHUser: "root", ListenPort: 40020, TargetIp: "localhost", TargetPort: 40020},
+		{SSHIp: "124.71.35.157", SSHPort: 22, SSHUser: "root", ListenPort: 40010, TargetIp: "localhost", TargetPort: 20010},
+		{SSHIp: "124.71.35.157", SSHPort: 22, SSHUser: "root", ListenPort: 40020, TargetIp: "localhost", TargetPort: 20020},
 		{SSHIp: "124.71.35.157", SSHPort: 22, SSHUser: "root", ListenPort: 40030, TargetIp: "localhost", TargetPort: 10006},
 	})
 }
