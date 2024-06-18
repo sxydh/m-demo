e7307bdf945b345bd211c3d0d97631a15ff3f4b3
sxydh <sxydhgg@gmail.com>
2024-06-18 19:17:59 +0800

    [UPDATE]

--- go-demo/tcp-demo/client-demo/app.go
+++ go-demo/tcp-demo/client-demo/app.go
@@ -2,45 +2,64 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
 )
 
+//goland:noinspection GoUnhandledErrorResult
 func main() {
 	/* 建立连接 */
-	conn, err := net.Dial("tcp", "localhost:8080")
+	address := "localhost:30010"
+	conn, err := net.Dial("tcp", address)
 	if err != nil {
-		fmt.Println("Error connecting: ", err)
+		log.Printf("Dial error: address=%v, err=%v", address, err)
 		return
 	}
-	defer func(conn net.Conn) {
-		err := conn.Close()
-		if err != nil {
-			fmt.Println("Error closing: ", err)
-		}
-	}(conn)
+	defer conn.Close()
 
+	/* 接收消息 */
+	go func() {
+		reader := bufio.NewReader(conn)
+		for {
+			bytes := make([]byte, 4)
+			_, err = io.ReadFull(reader, bytes)
+			if err != nil {
+				log.Printf("Read body length error, localAddr=%v， remoteAddr=%v", conn.LocalAddr(), conn.RemoteAddr())
+				return
+			}
+			bytes = make([]byte, binary.BigEndian.Uint64(bytes))
+			_, err = io.ReadFull(reader, bytes)
+			if err != nil {
+				log.Printf("Read body error, localAddr=%v， remoteAddr=%v", conn.LocalAddr(), conn.RemoteAddr())
+				return
+			}
+			log.Printf("Get message: msg=%v", string(bytes))
+		}
+	}()
+
+	/* 发送消息 */
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Enter message: ")
 		scanner.Scan()
 		input := scanner.Text()
 
-		/* 发送消息 */
-		_, err := conn.Write([]byte(input))
+		// 解决粘包问题
+		bytes := make([]byte, 4)
+		binary.BigEndian.PutUint64(bytes, uint64(len(input)))
+		_, err = conn.Write(bytes)
 		if err != nil {
-			fmt.Println("Error writing: ", err)
+			log.Printf("Write body length error: err=%v", err)
 			return
 		}
-
-		/* 接收消息 */
-		bytes := make([]byte, 1024)
-		_, err = conn.Read(bytes)
+		_, err = conn.Write([]byte(input))
 		if err != nil {
-			fmt.Println("Error reading: ", err)
+			log.Printf("Write body error: err=%v", err)
 			return
 		}
-		fmt.Println("Message reading: ", string(bytes))
 	}
 }
