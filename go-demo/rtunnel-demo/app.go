ff19797ab63ac7c1a0d9fd04c4729c6af782c592
sxydh <sxydhgg@gmail.com>
2024-06-19 10:05:12 +0800

    [UPDATE]

--- go-demo/rtunnel-demo/app.go
+++ go-demo/rtunnel-demo/app.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sxydh/mgo-util/ssh_utils"
+	"io"
+	"log"
+	"os"
+	"time"
 )
 
 /*
@@ -17,7 +22,23 @@ import (
  * go build -o rtunnel_demo.exe
  */
 
+//goland:noinspection GoUnhandledErrorResult
 func main() {
+	/* 日志配置 */
+	// 输出日期和时间
+	log.SetFlags(log.Ldate | log.Ltime)
+	// 输出到控制台和文件
+	logPath := "./logs"
+	_ = os.Mkdir(logPath, os.ModePerm)
+	logPath += "/" + time.Now().Format("2006-01-02") + ".log"
+	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		fmt.Printf("OpenFile error: logPath=%v, err=%v\r\n", logPath, err)
+	}
+	defer file.Close()
+	writer := io.MultiWriter(os.Stdout, file)
+	log.SetOutput(writer)
+
 	ssh_utils.NewReverseTunnel(&[]*ssh_utils.Tunnel{
 		{SSHIp: "124.71.35.157", SSHPort: 22, SSHUser: "root", ListenPort: 40010, TargetIp: "localhost", TargetPort: 20010},
 		{SSHIp: "124.71.35.157", SSHPort: 22, SSHUser: "root", ListenPort: 40020, TargetIp: "localhost", TargetPort: 20020},
