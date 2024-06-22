69a4facecd42335d52eb9d64f83aeae731918afb
sxydh <sxydhgg@gmail.com>
2024-06-22 18:39:53 +0800

    [UPDATE]

---
+++ javafx-demo/javafxquickstart-demo/src/main/java/cn/net/bhe/javafxquickstartdemo/TransformApp.java
@@ -0,0 +1,39 @@
+package cn.net.bhe.javafxquickstartdemo;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class TransformApp extends Application {
+
+    public static void launch(String[] args) {
+        Application.launch(args);
+    }
+
+    private Parent buildRoot() {
+        Rectangle box = new Rectangle(100, 50, Color.BLUE);
+        transform(box);
+        return new Pane(box);
+    }
+
+    private void transform(Rectangle box) {
+        box.setTranslateX(100);
+        box.setTranslateY(200);
+
+        box.setScaleX(1.5);
+        box.setScaleY(1.5);
+
+        box.setRotate(30);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setScene(new Scene(buildRoot(), 300, 300, Color.GRAY));
+        stage.show();
+    }
+
+}
---
+++ javafx-demo/javafxquickstart-demo/src/main/java/cn/net/bhe/javafxquickstartdemo/ui_control/ChoiceApp.java
@@ -0,0 +1,74 @@
+package cn.net.bhe.javafxquickstartdemo.ui_control;
+
+import cn.net.bhe.mutil.StrUtils;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+import javafx.util.StringConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChoiceApp extends Application {
+
+    public static void launch(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Asset Class:");
+        ChoiceBox<Pair<String, String>> choiceBox = new ChoiceBox<>();
+        choiceBox.setPrefWidth(200);
+        initChoice(choiceBox);
+        Button saveButton = new Button("Save");
+
+        HBox hbox = new HBox(
+                label,
+                choiceBox,
+                saveButton);
+        hbox.setSpacing(10.0);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setPadding(new Insets(40));
+
+        Scene scene = new Scene(hbox);
+
+        saveButton.setOnAction(evt -> System.out.println("saving " + choiceBox.getValue()));
+
+        primaryStage.setTitle("ChoicesApp");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void initChoice(ChoiceBox<Pair<String, String>> choiceBox) {
+        List<Pair<String, String>> items = new ArrayList<>();
+        items.add(new Pair<>("Equipment", "20000"));
+        items.add(new Pair<>("Furniture", "21000"));
+        items.add(new Pair<>("Investment", "22000"));
+
+        choiceBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(Pair<String, String> pair) {
+                return pair.getKey();
+            }
+
+            @Override
+            public Pair<String, String> fromString(String string) {
+                return null;
+            }
+        });
+
+        Pair<String, String> EMPTY_PAIR = new Pair<>(StrUtils.EMPTY, StrUtils.EMPTY);
+        choiceBox.getItems().add(EMPTY_PAIR);
+        choiceBox.getItems().addAll(items);
+        choiceBox.setValue(EMPTY_PAIR);
+    }
+
+}
