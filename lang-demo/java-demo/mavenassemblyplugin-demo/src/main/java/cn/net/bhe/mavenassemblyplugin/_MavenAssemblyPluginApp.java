f9838e212477e2c5dcf3a2b95f116b25439372ec
sxydh <sxydhgg@gmail.com>
2024-06-28 16:26:54 +0800

    [UPDATE]

--- lang-demo/java-demo/mavenassemblyplugin-demo/src/main/java/cn/net/bhe/mavenassemblyplugin/_MavenAssemblyPluginApp.java
+++ lang-demo/java-demo/mavenassemblyplugin-demo/src/main/java/cn/net/bhe/mavenassemblyplugin/_MavenAssemblyPluginApp.java
@@ -2,11 +2,13 @@ package cn.net.bhe.mavenassemblyplugin;
 
 import lombok.extern.log4j.Log4j2;
 
+import java.util.Arrays;
+
 @Log4j2
 public class _MavenAssemblyPluginApp {
 
     public static void main(String[] args) {
-        log.info(args);
+        log.info(Arrays.toString(args));
     }
 
 }
