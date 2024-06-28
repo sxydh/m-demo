1cff4f42b97744e852b54ecaa0935d021a99af14
sxydh <sxydhgg@gmail.com>
2024-06-28 13:54:09 +0800

    [UPDATE]

---
+++ lang-demo/java-demo/jdk-demo/src/main/java/cn/net/bhe/jdkdemo/_JdkApp.java
@@ -0,0 +1,12 @@
+package cn.net.bhe.jdkdemo;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class _JdkApp {
+
+    public static void main(String[] args) {
+        log.info(_JdkApp.class);
+    }
+
+}
