5a332c114d4108c5d286945feb191b47a0eb9e7b
sxydh <sxydhgg@gmail.com>
2024-06-28 16:24:41 +0800

    [UPDATE]

---
+++ lang-demo/java-demo/mavenassemblyplugin-demo/src/main/java/cn/net/bhe/mavenassemblyplugin/_MavenAssemblyPluginApp.java
@@ -0,0 +1,12 @@
+package cn.net.bhe.mavenassemblyplugin;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class _MavenAssemblyPluginApp {
+
+    public static void main(String[] args) {
+        log.info(args);
+    }
+
+}
