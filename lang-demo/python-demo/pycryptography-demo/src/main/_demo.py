fbb5d46e258eb628500a28f4e1a0515a26bd35d7
sxydh <sxydhgg@gmail.com>
2024-07-02 15:47:27 +0800

    [UPDATE]

--- lang-demo/python-demo/pycryptography-demo/src/main/_demo.py
+++ lang-demo/python-demo/pycryptography-demo/src/main/_demo.py
@@ -5,6 +5,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+# 不要删除
 salt = b'20240626'
 
 
