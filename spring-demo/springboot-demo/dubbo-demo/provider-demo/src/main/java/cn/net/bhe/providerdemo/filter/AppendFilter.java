3f0f2ba6666afa920220685735ffd108784546ea
sxydh <sxydhgg@gmail.com>
2024-07-06 16:24:14 +0800

    [UPDATE]

--- spring-demo/springboot-demo/dubbo-demo/provider-demo/src/main/java/cn/net/bhe/providerdemo/filter/AppendFilter.java
+++ spring-demo/springboot-demo/dubbo-demo/provider-demo/src/main/java/cn/net/bhe/providerdemo/filter/AppendFilter.java
@@ -1,13 +1,18 @@
 package cn.net.bhe.providerdemo.filter;
 
+import dubbodemo.api.HelloService;
+import lombok.extern.log4j.Log4j2;
 import org.apache.dubbo.rpc.*;
 
+@Log4j2
 public class AppendFilter implements Filter {
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         Result result = invoker.invoke(invocation);
-        result.setValue(result.getValue() + "'s customized " + AppendFilter.class);
+        if (invocation.getTargetServiceUniqueName().contains(HelloService.class.getSimpleName())) {
+            result.setValue(result.getValue() + "'s customized " + AppendFilter.class.getSimpleName());
+        }
         return result;
     }
 
