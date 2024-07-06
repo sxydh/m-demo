c1becbfd9cced89cecf5df0eaeccaa1e1af780b0
sxydh <sxydhgg@gmail.com>
2024-07-06 15:31:15 +0800

    [UPDATE]

--- spring-demo/springboot-demo/quickstart-demo/src/main/java/cn/net/bhe/quickstartdemo/_QuickStartApp.java
+++ spring-demo/springboot-demo/quickstart-demo/src/main/java/cn/net/bhe/quickstartdemo/_QuickStartApp.java
@@ -1,13 +1,16 @@
 package cn.net.bhe.quickstartdemo;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class _QuickStartApp {
 
     public static void main(String[] args) {
         SpringApplication.run(_QuickStartApp.class);
+        log.info("_QuickStartApp");
     }
 
 }
