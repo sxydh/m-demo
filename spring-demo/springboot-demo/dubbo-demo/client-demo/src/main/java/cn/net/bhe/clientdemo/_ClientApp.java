a1bb9d81d8ae467e56d0339cec653cf3cfeabb0e
sxydh <sxydhgg@gmail.com>
2024-07-06 15:08:14 +0800

    [UPDATE]

--- spring-demo/springboot-demo/dubbo-demo/client-demo/src/main/java/cn/net/bhe/clientdemo/_ClientApp.java
+++ spring-demo/springboot-demo/dubbo-demo/client-demo/src/main/java/cn/net/bhe/clientdemo/_ClientApp.java
@@ -1,13 +1,37 @@
 package cn.net.bhe.clientdemo;
 
+import cn.net.bhe.clientdemo.api.HelloService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableDubbo
 public class _ClientApp {
 
     public static void main(String[] args) {
         SpringApplication.run(_ClientApp.class);
+    }
+
+    @DubboReference
+    private HelloService helloService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    private void onApplicationReady() {
+        new Thread(() -> {
+            while (true) {
+                helloService.hello();
+                try {
+                    //noinspection BusyWait
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
 }
