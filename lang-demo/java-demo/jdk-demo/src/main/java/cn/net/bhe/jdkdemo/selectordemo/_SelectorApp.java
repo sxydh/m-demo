69d0f203a2e913e7371ea4146345e610b5de6eb0
sxydh <sxydhgg@gmail.com>
2024-07-07 21:50:43 +0800

    [UPDATE]

--- lang-demo/java-demo/jdk-demo/src/main/java/cn/net/bhe/jdkdemo/selectordemo/_SelectorApp.java
+++ lang-demo/java-demo/jdk-demo/src/main/java/cn/net/bhe/jdkdemo/selectordemo/_SelectorApp.java
@@ -1,9 +1,36 @@
 package cn.net.bhe.jdkdemo.selectordemo;
 
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 public class _SelectorApp {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        try (ServerSocketChannel serverChannel = ServerSocketChannel.open()) {
+            serverChannel.bind(new InetSocketAddress(8080));
+            serverChannel.configureBlocking(false);
+            try (Selector selector = Selector.open()) {
+                serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+                // noinspection InfiniteLoopStatement
+                while (true) {
+                    int i = selector.select(1000);
+                    if (i == 0) {
+                        continue;
+                    }
+                    Set<SelectionKey> keys = selector.selectedKeys();
+                    Iterator<SelectionKey> keyIterator = keys.iterator();
+                    while (keyIterator.hasNext()) {
+                        SelectionKey next = keyIterator.next();
+                        keyIterator.remove();
+                        System.out.println(next);
+                    }
+                }
+            }
+        }
     }
 
 }
