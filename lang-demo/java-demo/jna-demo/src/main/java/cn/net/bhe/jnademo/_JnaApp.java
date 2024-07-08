94399aec3bcc722e1efefd8adfdf580815d59c2d
sxydh <sxydhgg@gmail.com>
2024-07-08 11:22:45 +0800

    [UPDATE]

--- lang-demo/java-demo/jna-demo/src/main/java/cn/net/bhe/jnademo/_JnaApp.java
+++ lang-demo/java-demo/jna-demo/src/main/java/cn/net/bhe/jnademo/_JnaApp.java
@@ -2,9 +2,10 @@ package cn.net.bhe.jnademo;
 
 public class _JnaApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int port = JnaFs.INSTANCE.Fs();
         System.out.println(port);
+        Thread.currentThread().join();
     }
 
 }
