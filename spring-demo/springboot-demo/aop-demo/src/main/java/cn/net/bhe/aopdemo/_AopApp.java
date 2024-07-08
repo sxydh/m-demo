1fc1a0fc81cd1879266575e73caa79c5a389d1a5
sxydh <sxydhgg@gmail.com>
2024-07-08 20:06:51 +0800

    [UPDATE]

---
+++ spring-demo/springboot-demo/aop-demo/src/main/java/cn/net/bhe/aopdemo/_AopApp.java
@@ -0,0 +1,11 @@
+package cn.net.bhe.aopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class _AopApp {
+    public static void main(String[] args) {
+        SpringApplication.run(_AopApp.class);
+    }
+}
