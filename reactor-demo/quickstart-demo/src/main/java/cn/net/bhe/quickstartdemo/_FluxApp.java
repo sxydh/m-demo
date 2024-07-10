9b15b93188a66dcaa82616b075bb1907f6572619
sxydh <sxydhgg@gmail.com>
2024-07-10 23:56:40 +0800

    [UPDATE]

--- reactor-demo/quickstart-demo/src/main/java/cn/net/bhe/quickstartdemo/_FluxApp.java
+++ reactor-demo/quickstart-demo/src/main/java/cn/net/bhe/quickstartdemo/_FluxApp.java
@@ -1,7 +1,6 @@
 package cn.net.bhe.quickstartdemo;
 
 import cn.net.bhe.mutil.CpUtils;
-import cn.net.bhe.mutil.NumUtils;
 import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
@@ -9,30 +8,13 @@ import reactor.core.scheduler.Schedulers;
 import java.util.Map;
 
 @Log4j2
-public class FluxDemo {
+public class _FluxApp {
 
-    public static void main(String[] args) throws Exception {
-        Flux<Integer> flux2 = Flux.generate(synchronousSink -> {
-            Integer value = NumUtils.ranInt();
-            log.info("Produce -> {}", value);
-            synchronousSink.next(value);
-            sleep(1000);
-        });
-
-        flux2 = Flux.just(1, 2, 3, 4);
-
-        flux2.map(value -> value + 1)
-                .map(value -> value + 1)
-                .map(value -> value + 1)
-                .flatMap(value -> Flux.just(value + 1))
-                .subscribe(System.out::println);
-        Thread.currentThread().join();
-
-
+    public static void main(String[] args) {
         /* 序列生产者 */
         Flux<String> flux = Flux.generate(synchronousSink -> {
             String value = CpUtils.ranChnCp();
-            log.info("Produce -> {}", value);
+            log.info("generate: {}", value);
             synchronousSink.next(value);
             sleep(10);
         });
@@ -45,22 +27,22 @@ public class FluxDemo {
                         .map(pvalue -> {
                             sleep(15000);
                             Map<String, Object> map = Map.of(pvalue, pvalue.length());
-                            log.info("Map -> {}", map);
+                            log.info("map: {}", map);
                             return map.toString();
                         }));
 
         /* 序列消费者 */
         flux.subscribe(
-                value -> log.info("Consume -> {}", value),
-                error -> log.error("Error -> {}", error.getLocalizedMessage()),
-                () -> log.info("Complete"));
+                value -> log.info("consumer: {}", value),
+                error -> log.error("errorConsumer: {}", error.getLocalizedMessage()),
+                () -> log.info("completeConsumer"));
     }
 
     private static void sleep(long millis) {
         try {
             Thread.sleep(millis);
         } catch (Exception e) {
-            log.error(e.getLocalizedMessage(), e);
+            log.error(e);
         }
     }
 
