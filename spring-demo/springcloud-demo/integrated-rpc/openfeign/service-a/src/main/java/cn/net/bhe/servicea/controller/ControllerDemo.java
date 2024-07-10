fda9a5e9eafcb8ddc597a1b4807a29acde2cc98c
sxydh <sxydhgg@gmail.com>
2024-07-10 10:46:28 +0800

    [UPDATE]

---
+++ spring-demo/springcloud-demo/integrated-rpc/openfeign/service-a/src/main/java/cn/net/bhe/servicea/controller/ControllerDemo.java
@@ -0,0 +1,16 @@
+package cn.net.bhe.servicea.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ControllerDemo {
+
+    @GetMapping
+    public String get() {
+        return "Service A";
+    }
+
+}
