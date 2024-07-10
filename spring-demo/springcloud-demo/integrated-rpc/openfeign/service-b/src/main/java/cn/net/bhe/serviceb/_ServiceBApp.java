b4d8e6336aa0f59f942305dde7584d5e1550d9f2
sxydh <sxydhgg@gmail.com>
2024-07-10 11:18:28 +0800

    [UPDATE]

--- spring-demo/springcloud-demo/integrated-rpc/openfeign/service-b/src/main/java/cn/net/bhe/serviceb/_ServiceBApp.java
+++ spring-demo/springcloud-demo/integrated-rpc/openfeign/service-b/src/main/java/cn/net/bhe/serviceb/_ServiceBApp.java
@@ -3,6 +3,9 @@ package cn.net.bhe.serviceb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,12 @@ public class _ServiceBApp {
 
     public static void main(String[] args) {
         SpringApplication.run(_ServiceBApp.class);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- spring-demo/springcloud-demo/integrated-rpc/openfeign/service-b/src/main/java/cn/net/bhe/serviceb/controller/ControllerDemo.java
+++ spring-demo/springcloud-demo/integrated-rpc/openfeign/service-b/src/main/java/cn/net/bhe/serviceb/controller/ControllerDemo.java
@@ -1,16 +1,21 @@
 package cn.net.bhe.serviceb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/")
 public class ControllerDemo {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping
     public String get() {
-        return "Service B";
+        return "Service B " + restTemplate.getForObject("http://service-a/", String.class);
     }
 
 }
