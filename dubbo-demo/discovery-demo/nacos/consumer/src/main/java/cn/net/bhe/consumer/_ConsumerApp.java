f95b81b2a0c083ccc91d18db9d1233f0ac0abe6b
sxydh <sxydhgg@gmail.com>
2024-07-11 10:29:28 +0800

    [UPDATE]

--- dubbo-demo/discovery-demo/nacos/consumer/src/main/java/cn/net/bhe/consumer/_ConsumerApp.java
+++ dubbo-demo/discovery-demo/nacos/consumer/src/main/java/cn/net/bhe/consumer/_ConsumerApp.java
@@ -2,21 +2,25 @@ package cn.net.bhe.consumer;
 
 import cn.net.bhe.api.HelloService;
 import cn.net.bhe.mutil.StrUtils;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.bootstrap.DubboBootstrap;
 import org.apache.dubbo.config.bootstrap.builders.ReferenceBuilder;
+import org.apache.dubbo.config.bootstrap.builders.RegistryBuilder;
 
-@Log4j2
+@Slf4j
 public class _ConsumerApp {
 
     public static void main(String[] args) {
         ReferenceConfig<HelloService> reference = ReferenceBuilder.<HelloService>newBuilder()
                 .interfaceClass(HelloService.class)
-                .url("tri://localhost:50052?qos.port=22223")
                 .build();
 
         DubboBootstrap.getInstance()
+                .application(_ConsumerApp.class.getName())
+                .registry(RegistryBuilder.newBuilder()
+                        .address("nacos://localhost:8848")
+                        .build())
                 .reference(reference)
                 .start();
 
@@ -26,7 +30,7 @@ public class _ConsumerApp {
             try {
                 message = service.hello();
                 log.info(message);
-                //noinspection BusyWait
+                // noinspection BusyWait
                 Thread.sleep(1000);
             } catch (Throwable t) {
                 log.error(t.getLocalizedMessage(), t);
--- dubbo-demo/discovery-demo/nacos/provider/src/main/java/cn/net/bhe/provider/_ProviderApp.java
+++ dubbo-demo/discovery-demo/nacos/provider/src/main/java/cn/net/bhe/provider/_ProviderApp.java
@@ -3,6 +3,7 @@ package cn.net.bhe.provider;
 import cn.net.bhe.api.HelloService;
 import org.apache.dubbo.config.bootstrap.DubboBootstrap;
 import org.apache.dubbo.config.bootstrap.builders.ProtocolBuilder;
+import org.apache.dubbo.config.bootstrap.builders.RegistryBuilder;
 import org.apache.dubbo.config.bootstrap.builders.ServiceBuilder;
 
 public class _ProviderApp {
@@ -10,8 +11,17 @@ public class _ProviderApp {
     public static void main(String[] args) {
         DubboBootstrap.getInstance()
                 .application(_ProviderApp.class.getName())
-                .protocol(ProtocolBuilder.newBuilder().name("tri").port(50052).build())
-                .service(ServiceBuilder.newBuilder().interfaceClass(HelloService.class).ref(new HelloServiceImpl()).build())
+                .registry(RegistryBuilder.newBuilder()
+                        .address("nacos://localhost:8848")
+                        .build())
+                .protocol(ProtocolBuilder.newBuilder()
+                        .name("dubbo")
+                        .port(-1)
+                        .build())
+                .service(ServiceBuilder.newBuilder()
+                        .interfaceClass(HelloService.class)
+                        .ref(new HelloServiceImpl())
+                        .build())
                 .start()
                 .await();
     }
