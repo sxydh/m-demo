072bab49744fffb9412968e2c8c1b4ddadee5672
sxydh <sxydhgg@gmail.com>
2024-07-12 15:38:13 +0800

    [UPDATE]

--- dubbo-demo/discovery-demo/nacos/provider/src/main/java/cn/net/bhe/provider/filter/DemoFilter.java
+++ dubbo-demo/discovery-demo/nacos/provider/src/main/java/cn/net/bhe/provider/filter/DemoFilter.java
@@ -13,6 +13,8 @@ public class DemoFilter implements Filter {
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        String token = RpcContext.getServerContext().getAttachment("token");
+        log.info("Client token: {}", token);
         Result result = invoker.invoke(invocation);
         if (invocation.getTargetServiceUniqueName().contains(HelloService.class.getSimpleName())) {
             result.setValue(result.getValue() + " Provider's " + DemoFilter.class.getSimpleName());
