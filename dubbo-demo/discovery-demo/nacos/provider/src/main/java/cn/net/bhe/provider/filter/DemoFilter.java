474c1503da26b90d98b87d7cfcbae2c19f7d78ab
sxydh <sxydhgg@gmail.com>
2024-07-12 15:26:40 +0800

    [UPDATE]

--- dubbo-demo/discovery-demo/nacos/provider/src/main/java/cn/net/bhe/provider/filter/DemoFilter.java
+++ dubbo-demo/discovery-demo/nacos/provider/src/main/java/cn/net/bhe/provider/filter/DemoFilter.java
@@ -1,9 +1,15 @@
 package cn.net.bhe.provider.filter;
 
 import cn.net.bhe.api.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.rpc.*;
 
+@Slf4j
 public class DemoFilter implements Filter {
+
+    public DemoFilter() {
+        log.info("{}: new", DemoFilter.class.getSimpleName());
+    }
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
