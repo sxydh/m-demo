bd9342069260bc23c74fd8999b5a2edc0be2be34
sxydh <sxydhgg@gmail.com>
2024-07-12 13:59:09 +0800

    [UPDATE]

--- dubbo-demo/discovery-demo/nacos/provider/src/main/java/cn/net/bhe/provider/HelloServiceImpl.java
+++ dubbo-demo/discovery-demo/nacos/provider/src/main/java/cn/net/bhe/provider/HelloServiceImpl.java
@@ -3,17 +3,10 @@ package cn.net.bhe.provider;
 import cn.net.bhe.api.HelloService;
 import cn.net.bhe.mutil.NmUtils;
 
-import java.util.Random;
-
 public class HelloServiceImpl implements HelloService {
-
-    private final Random random = new Random();
 
     @Override
     public String hello() {
-        if (random.nextBoolean()) {
-            throw new RuntimeException();
-        }
         return NmUtils.randomName();
     }
 
