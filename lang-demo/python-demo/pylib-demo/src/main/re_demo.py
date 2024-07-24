7e419dff6c5046c4ff1d31180fdca1f7661013d7
sxydh <sxydhgg@gmail.com>
2024-07-24 11:25:59 +0800

    [UPDATE]

--- lang-demo/python-demo/pylib-demo/src/main/re_demo.py
+++ lang-demo/python-demo/pylib-demo/src/main/re_demo.py
@@ -6,7 +6,11 @@ from file_demo import vim
 
 
 def is_w(c):
-    return re.match(r'[0-9a-zA-Z]|[ `~!@#$%^&*()\-_=+\[{\]}\\|;:\'",<.>/?]', c)
+    return not c or re.match(r'[0-9a-zA-Z]|[`~!@#$%^&*()\-_=+\[{\]}\\|;:\'",<.>/?]', c)
+
+
+def is_n(c):
+    return c != '\r' and c != '\n' and c != '\r\n'
 
 
 if __name__ == '__main__':
@@ -20,7 +24,11 @@ if __name__ == '__main__':
     for fc in fs:
         b1 = not is_w(fc) and is_w(prev)
         b2 = is_w(fc) and not is_w(prev)
-        if b1 or b2:
+        b3 = b1 or b2
+        b4 = prev and prev != ' ' and not is_n(prev)
+        b5 = fc != ' ' and not is_n(fc)
+        b = b3 and b4 and b5
+        if b:
             res = res + ' ' + fc
         else:
             res = res + fc
