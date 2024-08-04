3ae565118edd973443cb076a790a67360d7a4a91
sxydh <sxydhgg@gmail.com>
2024-08-04 18:23:41 +0800

    [UPDATE]

---
+++ spring-demo/springboot-demo/redis-demo/src/main/java/cn/net/bhe/redisdemo/_RedisApp.java
@@ -0,0 +1,13 @@
+package cn.net.bhe.redisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class _RedisApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(_RedisApp.class);
+    }
+
+}
