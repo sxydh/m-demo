b62fdb6d8310d0936c26bb2e31250ed141d96ff0
sxydh <sxydhgg@gmail.com>
2024-08-17 19:24:38 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/util/common.py
+++ lang-demo/python-demo/selenium-demo/src/main/util/common.py
@@ -1,13 +1,13 @@
-def append(r):
-    with open('tmp/output.txt', mode='a', encoding='utf-8') as o:
+def append(r, f='output.txt'):
+    with open(f'tmp/{f}', mode='a', encoding='utf-8') as o:
         o.write(r + '\r')
 
 
-def append_e(r):
-    with open('tmp/error.txt', mode='a', encoding='utf-8') as o:
+def append_e(r, f='error.txt'):
+    with open(f'tmp/{f}', mode='a', encoding='utf-8') as o:
         o.write(r + '\r')
 
 
-def read_rows(f):
-    with open(f'input/{f}', mode='r', encoding='utf-8') as i:
+def read_rows(f='input.txt'):
+    with open(f'tmp/{f}', mode='r', encoding='utf-8') as i:
         return i.read().split('\r')
