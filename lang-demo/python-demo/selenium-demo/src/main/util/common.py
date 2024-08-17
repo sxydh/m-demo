59e1e232ad3b008af3dbd734c32960db48c4ff10
sxydh <sxydhgg@gmail.com>
2024-08-17 18:35:52 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/util/common.py
+++ lang-demo/python-demo/selenium-demo/src/main/util/common.py
@@ -6,3 +6,8 @@ def append(r):
 def append_e(r):
     with open('tmp/error.txt', mode='a', encoding='utf-8') as o:
         o.write(r + '\r')
+
+
+def read_rows():
+    with open('tmp/input.txt', mode='r', encoding='utf-8') as i:
+        return i.read().split('\r')
