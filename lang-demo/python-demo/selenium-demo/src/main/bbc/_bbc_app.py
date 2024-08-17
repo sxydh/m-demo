331f856e28412196260d20577f00f1fbcee49302
sxydh <sxydhgg@gmail.com>
2024-08-17 13:11:58 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/bbc/_bbc_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/bbc/_bbc_app.py
@@ -2,7 +2,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
 from src.main.util.chrome import ChromeCli
-from src.main.util.common import write
+from src.main.util.common import append
 
 if __name__ == '__main__':
     chromeCli = ChromeCli(url='https://www.bbc.com/news', proxy='127.0.0.1:10809')
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         for (i, li) in enumerate(lis):
             nav = chromeCli.find_element(src=driver, by=By.CSS_SELECTOR, value='[data-testid="level2-navigation-container"]')
             lis = chromeCli.find_elements(src=nav, by=By.CSS_SELECTOR, value='li')
-            write(lis[i].get_attribute('innerText'))
+            append(lis[i].get_attribute('innerText'))
             ActionChains(driver).move_to_element(lis[i]).click().perform()
             while True:
                 grid = chromeCli.find_element(src=driver, by=By.CSS_SELECTOR, value='[data-testid="alaska-grid"]')
@@ -22,8 +22,8 @@ if __name__ == '__main__':
                 for link in links:
                     headline = chromeCli.find_element(src=link, by=By.CSS_SELECTOR, value='[data-testid="card-headline"]')
                     description = chromeCli.find_element(src=link, by=By.CSS_SELECTOR, value='[data-testid="card-description"]')
-                    write('    ' + headline.get_attribute('innerText'))
-                    write('        ' + description.get_attribute('innerText'))
+                    append('    ' + headline.get_attribute('innerText'))
+                    append('        ' + description.get_attribute('innerText'))
                 nextButton = chromeCli.find_element(src=driver, by=By.CSS_SELECTOR, value='[data-testid="pagination-next-button"]')
                 if nextButton.get_attribute('disabled') == 'true':
                     break
