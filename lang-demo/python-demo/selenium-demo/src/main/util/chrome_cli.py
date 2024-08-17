48a9b62fc80d355816a66cc50d7934fb0798f357
sxydh <sxydhgg@gmail.com>
2024-08-17 14:17:32 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/util/chrome_cli.py
+++ lang-demo/python-demo/selenium-demo/src/main/util/chrome_cli.py
@@ -24,23 +24,19 @@ class ChromeCli:
         self.driver.maximize_window()
         self.driver.get(url)
 
-    def find_element_d(self, by, value, timeout=10, count=3):
-        return self.find_element(src=self.driver, by=by, value=value, timeout=timeout, count=count)
+    def find_element_d(self, by, value, timeout=10, count=3, raise_e=True):
+        return self.find_element(src=self.driver, by=by, value=value, timeout=timeout, count=count, raise_e=raise_e)
 
-    def find_element(self, src, by, value, timeout=10, count=3):
-        while count > 0:
-            try:
-                WebDriverWait(self.driver, timeout).until(ec.presence_of_element_located((by, value)))
-                return src.find_element(by=by, value=value)
-            except Exception as e:
-                count -= 1
-                print(e)
-        raise Exception(f'by={by}, value={value} not found')
+    def find_element(self, src, by, value, timeout=10, count=3, raise_e=True):
+        arr = self.find_elements(src=src, by=by, value=value, timeout=timeout, count=count, raise_e=raise_e)
+        if len(arr) != 0:
+            return arr[0]
+        return None
 
-    def find_elements_d(self, by, value, timeout=10, count=3):
-        return self.find_elements(src=self.driver, by=by, value=value, timeout=timeout, count=count)
+    def find_elements_d(self, by, value, timeout=10, count=3, raise_e=True):
+        return self.find_elements(src=self.driver, by=by, value=value, timeout=timeout, count=count, raise_e=raise_e)
 
-    def find_elements(self, src, by, value, timeout=10, count=3):
+    def find_elements(self, src, by, value, timeout=10, count=3, raise_e=True):
         while count > 0:
             try:
                 WebDriverWait(self.driver, timeout).until(ec.presence_of_element_located((by, value)))
@@ -48,7 +44,9 @@ class ChromeCli:
             except Exception as e:
                 count -= 1
                 print(e)
-        raise Exception(f'by={by}, value={value} not found')
+        if raise_e:
+            raise Exception(f'by={by}, value={value} not found')
+        return []
 
     def click(self, ele):
         ActionChains(self.driver).move_to_element(ele).click().perform()
