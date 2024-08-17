be871ccf09a95e36089ef31aa4100b62a6acaae9
sxydh <sxydhgg@gmail.com>
2024-08-17 20:26:21 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/boss/_boss_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/boss/_boss_app.py
@@ -85,7 +85,7 @@ def pull_jobs():
 
                         # 解析内容
                         login_close()
-                        jobs = chrome_cli.find_elements_d(by=By.CSS_SELECTOR, value='.job-list-box li')
+                        jobs = chrome_cli.find_elements_d(by=By.CSS_SELECTOR, value='.job-list-box .job-card-wrapper')
                         for job in jobs:
                             append(f'###########{job.get_attribute('innerHTML')}')
                         break
