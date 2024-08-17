ab806c1869a4b39f8337494090c3d69b4b1f5c5c
sxydh <sxydhgg@gmail.com>
2024-08-17 22:34:40 +0800

    [UPDATE]

--- lang-demo/python-demo/uc-demo/src/main/boss/_boss_app.py
+++ lang-demo/python-demo/uc-demo/src/main/boss/_boss_app.py
@@ -72,21 +72,23 @@ def pull_jobs():
         for query in queries:
             if f'{city},{query}' in jobs:
                 continue
+            if random.choice([False, True, False]):
+                chrome_cli.get(random.choice([
+                    'https://www.zhipin.com/web/geek/job-recommend',
+                    'https://www.zhipin.com/gongsi',
+                    'https://www.zhipin.com/overseas/',
+                    'https://app.zhipin.com/'
+                ]))
+                sleep(1)
+
             # 分页循环
             page = 1
             n = 1
             while page <= n:
+
                 # 重试循环
                 while True:
                     try:
-                        if random.choice([False, True, False]):
-                            chrome_cli.get(random.choice([
-                                'https://www.zhipin.com/web/geek/job-recommend',
-                                'https://www.zhipin.com/gongsi',
-                                'https://www.zhipin.com/overseas/',
-                                'https://app.zhipin.com/'
-                            ]))
-                            sleep(1)
                         chrome_cli.get(f'https://www.zhipin.com/web/geek/job?query={quote(query)}&city={quote(city)}&page={page}')
                         job_empty_box = chrome_cli.find_element_d(by=By.CSS_SELECTOR, value='.job-empty-box', timeout=1, raise_e=False)
                         if job_empty_box is not None:
