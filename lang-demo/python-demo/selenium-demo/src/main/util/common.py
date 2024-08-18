6d953671bcc96d661dc7b49b1c2fefe0fe3d140c
sxydh <sxydhgg@gmail.com>
2024-08-18 08:38:58 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/util/common.py
+++ lang-demo/python-demo/selenium-demo/src/main/util/common.py
@@ -1,21 +1,28 @@
 import datetime
 
 
-def append(r, f='output.txt'):
-    with open(f'tmp/{f}', mode='a', encoding='utf-8') as o:
-        o.write(r + '\n')
-
-
-def append_e(r, f='error.txt'):
-    with open(f'tmp/{f}', mode='a', encoding='utf-8') as o:
-        o.write(r + '\n')
-
-
-def read_rows(f='input.txt'):
-    with open(f'tmp/{f}', mode='r', encoding='utf-8') as i:
-        return i.read().split('\n')
-
-
 def add_days(ds, delta):
     d = datetime.datetime.strptime(ds, '%Y-%m-%d')
     return (d + datetime.timedelta(days=delta)).strftime('%Y-%m-%d')
+
+
+def write(r, mode, f):
+    with open(f'tmp/{f}', mode=mode, encoding='utf-8') as o:
+        o.write(r)
+
+
+def append(r, f='output.txt'):
+    write(r=f'{r}\n', mode='a', f=f)
+
+
+def append_e(r, f='error.txt'):
+    append(r=r, f=f)
+
+
+def read(f):
+    with open(f'tmp/{f}', mode='r', encoding='utf-8') as i:
+        return i.read()
+
+
+def read_rows(f='input.txt'):
+    return read(f).split('\n')
