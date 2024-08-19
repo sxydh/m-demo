33d3c498c5df2d9b123a48b99ae160e4d919e0b5
sxydh <sxydhgg@gmail.com>
2024-08-20 07:08:25 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/bbc/_bbc_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/bbc/_bbc_app.py
@@ -4,25 +4,25 @@ from src.main.util.cli import Cli
 from src.main.util.common import append
 
 if __name__ == '__main__':
-    chrome_cli = Cli(proxy='127.0.0.1:10809')
+    cli = Cli(proxy='127.0.0.1:10809')
 
     while True:
-        nav = chrome_cli.find_element_d(by=By.CSS_SELECTOR, value='[data-testid="level2-navigation-container"]')
-        lis = chrome_cli.find_elements(src=nav, by=By.CSS_SELECTOR, value='li')
+        nav = cli.find_element_d(by=By.CSS_SELECTOR, value='[data-testid="level2-navigation-container"]')
+        lis = cli.find_elements(src=nav, by=By.CSS_SELECTOR, value='li')
         for (i, li) in enumerate(lis):
-            nav = chrome_cli.find_element_d(by=By.CSS_SELECTOR, value='[data-testid="level2-navigation-container"]')
-            lis = chrome_cli.find_elements(src=nav, by=By.CSS_SELECTOR, value='li')
+            nav = cli.find_element_d(by=By.CSS_SELECTOR, value='[data-testid="level2-navigation-container"]')
+            lis = cli.find_elements(src=nav, by=By.CSS_SELECTOR, value='li')
             append(lis[i].get_attribute('innerText'))
-            chrome_cli.click(lis[i])
+            cli.click(lis[i])
             while True:
-                grid = chrome_cli.find_element_d(by=By.CSS_SELECTOR, value='[data-testid="alaska-grid"]')
-                links = chrome_cli.find_elements(src=grid, by=By.CSS_SELECTOR, value='[data-testid="internal-link"]')
+                grid = cli.find_element_d(by=By.CSS_SELECTOR, value='[data-testid="alaska-grid"]')
+                links = cli.find_elements(src=grid, by=By.CSS_SELECTOR, value='[data-testid="internal-link"]')
                 for link in links:
-                    headline = chrome_cli.find_element(src=link, by=By.CSS_SELECTOR, value='[data-testid="card-headline"]')
-                    description = chrome_cli.find_element(src=link, by=By.CSS_SELECTOR, value='[data-testid="card-description"]')
+                    headline = cli.find_element(src=link, by=By.CSS_SELECTOR, value='[data-testid="card-headline"]')
+                    description = cli.find_element(src=link, by=By.CSS_SELECTOR, value='[data-testid="card-description"]')
                     append(f'    {headline.get_attribute('innerText')}')
                     append(f'        {description.get_attribute('innerText')}')
-                next_button = chrome_cli.find_element_d(by=By.CSS_SELECTOR, value='[data-testid="pagination-next-button"]')
+                next_button = cli.find_element_d(by=By.CSS_SELECTOR, value='[data-testid="pagination-next-button"]')
                 if next_button.get_attribute('disabled') == 'true':
                     break
-                chrome_cli.click(next_button)
+                cli.click(next_button)
