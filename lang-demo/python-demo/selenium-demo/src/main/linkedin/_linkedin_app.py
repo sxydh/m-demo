cb6581c1bd63f77eb0c7d51e043e534543dc70a8
sxydh <sxydhgg@gmail.com>
2024-08-20 07:11:19 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/linkedin/_linkedin_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/linkedin/_linkedin_app.py
@@ -7,7 +7,14 @@ from src.main.util.cli import Cli
 from src.main.util.common import append_e, read_rows
 
 
-def pull_jobs():
+def in_login(cli: Cli):
+    login_form = cli.find_element_d(by=By.CSS_SELECTOR, value='.login__form_action_container', timeout=0, count=1, raise_e=False)
+    if login_form is not None:
+        return True
+    return False
+
+
+def pull_jobs(cli: Cli):
     locations = read_rows('locations')
     keywords = read_rows('keywords')
 
@@ -22,20 +29,20 @@ def pull_jobs():
                 # 重试循环
                 while True:
                     try:
-                        chrome_cli.get(f'https://www.linkedin.com/')
-                        if in_login():
+                        cli.get(f'https://www.linkedin.com/')
+                        if in_login(cli):
                             continue
-                        menu_jobs = chrome_cli.find_element_d(by=By.CSS_SELECTOR, value='[data-tracking-control-name="guest_homepage-basic_guest_nav_menu_jobs"]')
-                        chrome_cli.click(menu_jobs)
+                        menu_jobs = cli.find_element_d(by=By.CSS_SELECTOR, value='[data-tracking-control-name="guest_homepage-basic_guest_nav_menu_jobs"]')
+                        cli.click(menu_jobs)
 
-                        job_list = chrome_cli.find_elements_d(by=By.CSS_SELECTOR, value='.jobs-search__results-list li', timeout=1, count=3)
+                        job_list = cli.find_elements_d(by=By.CSS_SELECTOR, value='.jobs-search__results-list li', timeout=1, count=3)
                         for job in job_list:
                             sleep(random.choice([1, 1, 2, 2, 3]))
-                            chrome_cli.move_to_element(job)
-                            chrome_cli.click(job)
-                            show_more = chrome_cli.find_element_d(by=By.CSS_SELECTOR, value='[data-tracking-control-name="public_jobs_show-more-html-btn"]')
+                            cli.move_to_element(job)
+                            cli.click(job)
+                            show_more = cli.find_element_d(by=By.CSS_SELECTOR, value='[data-tracking-control-name="public_jobs_show-more-html-btn"]')
                             sleep(random.choice([1, 1, 2, 2, 3]))
-                            chrome_cli.click(show_more)
+                            cli.click(show_more)
                         break
                     except Exception as e:
                         append_e(str(e))
@@ -45,15 +52,10 @@ def pull_jobs():
                 sleep(1)
 
 
+def start():
+    cli = Cli(undetected=True, headless=False, proxy='127.0.0.1:10809')
+    pull_jobs(cli)
+
+
 if __name__ == '__main__':
-    chrome_cli = Cli(undetected=True, headless=False, proxy='127.0.0.1:10809')
-
-
-    def in_login():
-        login_form = chrome_cli.find_element_d(by=By.CSS_SELECTOR, value='.login__form_action_container', timeout=0, count=1, raise_e=False)
-        if login_form is not None:
-            return True
-        return False
-
-
-    pull_jobs()
+    start()
