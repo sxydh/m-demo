2ccc2ff5305927ad712f5737440188b49a6ac08c
sxydh <sxydhgg@gmail.com>
2024-08-21 21:48:28 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/ssq/_ssq_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/ssq/_ssq_app.py
@@ -35,7 +35,6 @@ def start():
         cli.click(jg_an03_2)
 
         # 解析结果
-        r = ''
         while True:
             try:
                 sleep(0.5)
@@ -43,7 +42,7 @@ def start():
                 trs = cli.find_elements(src=tbody, by=By.TAG_NAME, value='tr')
                 for tr in trs:
                     tds = cli.find_elements(src=tr, by=By.TAG_NAME, value='td')
-                    r += f'\'{tds[0].get_attribute('innerText')}\', '
+                    r = f'\'{tds[0].get_attribute('innerText')}\', '
                     spans = cli.find_elements(src=tds[2], by=By.TAG_NAME, value='span')
                     for span in spans:
                         r += f'\'{span.get_attribute('innerText')}\', '
