75dcafc67074894d7a3115aae2cb47524b4920ae
sxydh <sxydhgg@gmail.com>
2024-09-09 01:23:54 +0800

    [UPDATE]

--- spring-demo/springboot-demo/mongodb-demo/src/main/java/cn/net/bhe/mongodbdemo/web/UserController.java
+++ spring-demo/springboot-demo/mongodb-demo/src/main/java/cn/net/bhe/mongodbdemo/web/UserController.java
@@ -2,14 +2,20 @@ package cn.net.bhe.mongodbdemo.web;
 
 import cn.net.bhe.mongodbdemo.dao.dto.UserDTO;
 import cn.net.bhe.mongodbdemo.service.UserService;
+import cn.net.bhe.mutil.NmUtils;
+import cn.net.bhe.mutil.NumUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Scanner;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @Resource
@@ -23,6 +29,30 @@ public class UserController {
     @PostMapping
     public void addUser(UserDTO dto) {
         userService.addUser(dto);
+    }
+
+    @PostConstruct
+    public void init() {
+        new Thread(() -> {
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    String line = scanner.nextLine();
+                    if ("1".equals(line)) {
+                        UserDTO userDTO = new UserDTO();
+                        userDTO.setId(System.currentTimeMillis() + "");
+                        userDTO.setName(NmUtils.randomName());
+                        userDTO.setAge(NumUtils.ranInt());
+                        this.addUser(userDTO);
+                    } else if ("2".equals(line)) {
+                        UserDTO userDTO = new UserDTO();
+                        List<UserDTO> dtoList = this.listUsers(userDTO);
+                        System.out.println(dtoList);
+                    }
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
+        }).start();
     }
 
 }
