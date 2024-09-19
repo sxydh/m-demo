6ff3e25201a156b50ae6f3a2b6506806a15bb6b3
sxydh <sxydhgg@gmail.com>
2024-09-19 15:07:10 +0800

    [UPDATE]

---
+++ lang-demo/python-demo/selenium-demo/src/main/anjuke/_anjuke_app.py
@@ -0,0 +1,62 @@
+from selenium.webdriver.common.by import By
+
+from src.main.util.cli import Cli
+from src.main.util.common import append_e, append, read_rows, truncate
+
+
+def close_login(cli: Cli):
+    pass
+
+
+def pull_cities(force=False):
+    cities = read_rows(f='cities')
+    if not force and len(cities) > 0:
+        return
+    else:
+        truncate(f='cities')
+
+    cli = Cli(undetected=False,
+              images_disabled=False,
+              headless=False)
+    while True:
+        try:
+            cli.get('https://www.anjuke.com/sy-city.html?from=HomePage_City')
+            close_login(cli)
+            cities = cli.find_elements_d(by=By.CSS_SELECTOR, value='.ajk-city-cell.is-letter li a')
+            for city in cities:
+                append(f='cities', r=city.get_attribute('href'))
+            break
+        except Exception as e:
+            append_e(str(e))
+    cli.close()
+
+
+def pull_new_houses(force=False):
+    cities = read_rows(f='cities')
+    if len(cities) == 0:
+        return
+    new_houses = read_rows(f='new_houses')
+    if not force and len(new_houses) > 0:
+        return
+    else:
+        truncate(f='new_houses')
+
+    cli = Cli(undetected=False,
+              images_disabled=False,
+              headless=False)
+    for city in cities:
+        try:
+            cli.get(city)
+            close_login(cli)
+            nav_list = cli.find_elements_d(by=By.CSS_SELECTOR, value='.nav-channel-list li a', timeout=5, count=1)
+            new_house = nav_list[0].get_attribute('href')
+            append(f='new_houses', r=new_house)
+        except Exception as e:
+            append_e(f='new_houses_error', r=city)
+            append_e(str(e))
+    cli.close()
+
+
+if __name__ == '__main__':
+    pull_cities()
+    pull_new_houses()
