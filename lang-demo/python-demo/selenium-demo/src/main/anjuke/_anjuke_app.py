e6d76f408acbdd0005d19c92aec8d01bcc2badb6
sxydh <sxydhgg@gmail.com>
2024-09-19 23:32:06 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/anjuke/_anjuke_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/anjuke/_anjuke_app.py
@@ -50,7 +50,7 @@ def pull_new_houses(force=False):
             close_login(cli)
             nav_list = cli.find_elements_d(by=By.CSS_SELECTOR, value='.nav-channel-list li a', timeout=5, count=1)
             href = nav_list[0].get_attribute('href')
-            if href.index('/sale/') >= 0:
+            if '/sale/' in href:
                 continue
             append(f='new_houses', r=href)
         except Exception as e:
@@ -77,9 +77,6 @@ def pull_mods(force=False):
         for new_house in new_houses:
             try:
                 new_house = new_house.split('?')[0]
-                if new_house.index('sale') >= 0:
-                    conn.execute(f'insert into mods(new_house) values(\'{new_house}\')')
-                    continue
                 cli.get(f'{new_house}loupan/all/s1/')
                 close_login(cli)
                 empty = cli.find_element_d(by=By.CSS_SELECTOR, value='.empty', timeout=0, count=1, raise_e=False)
