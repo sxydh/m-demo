b9a830a7f86c9c7c7b273534c567ea5ba34ed542
sxydh <sxydhgg@gmail.com>
2024-09-20 22:19:17 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/stats/stats/spiders/tjbz.py
+++ lang-demo/python-demo/scrapy-demo/stats/stats/spiders/tjbz.py
@@ -1,3 +1,4 @@
+from time import sleep
 from typing import Any
 from urllib.parse import urljoin
 
@@ -16,31 +17,23 @@ class TjbzSpider(scrapy.Spider):
     def parse(self, response: Response, **kwargs: Any):
         provinces = response.css(".provincetr a")
         for province in provinces:
-            append(f='province', r=', '.join(province.css("::text").getall()))
+            append(f='province', r=province.css("::text").getall())
             yield scrapy.Request(urljoin(response.url, province.css("::attr(href)").get()), callback=self.parse_city)
+        sleep(9999999)
 
     def parse_city(self, response: Response):
-        cities = response.css(".citytr")
+        cities = response.css(".citytr a[href]")
         for city in cities:
-            city = city.css('a')
-            if city:
-                city = city[0]
-                append(f='city', r=', '.join(city.css("::text").getall()))
-                yield scrapy.Request(urljoin(response.url, city.css("::attr(href)").get()), callback=self.parse_county)
+            append(f='city', r=city.css("::text").getall())
+            yield scrapy.Request(urljoin(response.url, city.css("::attr(href)").get()), callback=self.parse_county)
 
     def parse_county(self, response: Response):
-        counties = response.css(".countytr")
+        counties = response.css(".countytr a[href]")
         for county in counties:
-            county = county.css('a')
-            if county:
-                county = county[0]
-                append(f='county', r=', '.join(county.css("::text").getall()))
-                yield scrapy.Request(urljoin(response.url, county.css("::attr(href)").get()), callback=self.parse_town)
+            append(f='county', r=county.css("::text").getall())
+            yield scrapy.Request(urljoin(response.url, county.css("::attr(href)").get()), callback=self.parse_town)
 
     def parse_town(self, response: Response):
-        towns = response.css(".towntr")
+        towns = response.css(".towntr a[href]")
         for town in towns:
-            town = town.css('a')
-            if town:
-                town = town[0]
-                append(f='town', r=', '.join(town.css("::text").getall()))
+            append(f='town', r=town.css("::text").getall())
