08d2225c2d024c60266dcdc09c14c5946167887f
sxydh <sxydhgg@gmail.com>
2024-09-20 22:06:43 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/stats/stats/spiders/tjbz.py
+++ lang-demo/python-demo/scrapy-demo/stats/stats/spiders/tjbz.py
@@ -22,19 +22,25 @@ class TjbzSpider(scrapy.Spider):
     def parse_city(self, response: Response):
         cities = response.css(".citytr")
         for city in cities:
-            city = city.css('a')[0]
-            append(f='city', r=', '.join(city.css("::text").getall()))
-            yield scrapy.Request(urljoin(response.url, city.css("::attr(href)").get()), callback=self.parse_county)
+            city = city.css('a')
+            if city:
+                city = city[0]
+                append(f='city', r=', '.join(city.css("::text").getall()))
+                yield scrapy.Request(urljoin(response.url, city.css("::attr(href)").get()), callback=self.parse_county)
 
     def parse_county(self, response: Response):
         counties = response.css(".countytr")
         for county in counties:
-            county = county.css('a')[0]
-            append(f='county', r=', '.join(county.css("::text").getall()))
-            yield scrapy.Request(urljoin(response.url, county.css("::attr(href)").get()), callback=self.parse_town)
+            county = county.css('a')
+            if county:
+                county = county[0]
+                append(f='county', r=', '.join(county.css("::text").getall()))
+                yield scrapy.Request(urljoin(response.url, county.css("::attr(href)").get()), callback=self.parse_town)
 
     def parse_town(self, response: Response):
         towns = response.css(".towntr")
         for town in towns:
-            town = town.css('a')[0]
-            append(f='town', r=', '.join(town.css("::text").getall()))
+            town = town.css('a')
+            if town:
+                town = town[0]
+                append(f='town', r=', '.join(town.css("::text").getall()))
