afc94b76e305c804ce0ab687beb773553c9be570
sxydh <sxydhgg@gmail.com>
2024-09-22 16:35:58 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/tjbz/tjbz/pipelines.py
+++ lang-demo/python-demo/scrapy-demo/tjbz/tjbz/pipelines.py
@@ -15,13 +15,13 @@ class TjbzPipeline:
 
     def open_spider(self, spider):
         self.conn = get_sqlite_connection()
-        self.conn.execute("create table if not exists tjbz(code text, name text, parent_code text, url text, remark text)")
+        self.conn.execute("create table if not exists tjbz(code text, name text, parent_code text, url text, level, remark text)")
         self.conn.execute("delete from tjbz where 1 = 1")
         self.conn.commit()
 
     def process_item(self, item, spider):
-        self.conn.execute("insert into tjbz(code, name, parent_code, url, remark) values (?, ?, ?, ?, ?)",
-                          (item["code"], item["name"], item.get("parent_code", None), item.get("url", None), item.get("remark", None)))
+        self.conn.execute("insert into tjbz(code, name, parent_code, url, remark) values (?, ?, ?, ?, ?, ?)",
+                          (item["code"], item["name"], item.get("parent_code", None), item.get("url", None), item["level"], item.get("remark", None)))
         self.conn.commit()
 
     def close_spider(self, spider):
