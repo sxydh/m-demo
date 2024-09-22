f91170cfc458fcfd8e32df226408fdbc3a78f4a7
sxydh <sxydhgg@gmail.com>
2024-09-22 16:02:12 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/tjbz/tjbz/spiders/example.py
+++ lang-demo/python-demo/scrapy-demo/tjbz/tjbz/spiders/example.py
@@ -9,41 +9,39 @@ from scrapy.http import Response
 from tjbz.items import TjbzItem
 
 
+# noinspection DuplicatedCode
 class ExampleSpider(scrapy.Spider):
     name = "example"
     allowed_domains = ["stats.gov.cn"]
     start_urls = ["https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/index.html"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        provinces = response.css(".provincetr a")
-        return self.parse_do(response, provinces, self.parse_city)
-
-    def parse_city(self, response: Response):
-        cities = response.css(".citytr")
-        self.parse_do(response, cities, self.parse_county)
-
-    def parse_county(self, response: Response):
-        counties = response.css(".countytr")
-        self.parse_do(response, counties, self.parse_town)
-
-    def parse_town(self, response: Response):
-        towns = response.css(".towntr")
-        self.parse_do(response, towns, None)
-
-    def parse_do(self, response: Response, arr, callback):
-        for ele in arr:
-            alist = ele.css("a")
+        trs = response.css(".provincetr")
+        trs += response.css(".citytr")
+        trs += response.css(".countytr")
+        trs += response.css(".towntr")
+        for tr in trs:
+            tds = tr.css("td")
             item = TjbzItem()
-            item["code"] = alist[0].css("::text").get()
-            item["name"] = alist[-1].css("::text").get()
-            item["url"] = response.urljoin(alist[-1].css("::attr(href)").get())
-            if "meta_parent" in response.meta:
+            if len(tds) == 1:
+                item["name"] = tds[0].css("::text").get().strip()
+                al = tds[0].css("a")
+                if len(al) > 0:
+                    href = al[0].css("::attr(href)").get()
+                    item["code"] = href.replace('.html', '')
+                    item["url"] = response.urljoin(href)
+            else:
+                item["code"] = tds[0].css("::text").get().strip()
+                item["name"] = tds[-1].css("::text").get().strip()
                 item["parent_code"] = response.meta["meta_parent"]["code"]
+                al = tds[0].css("a")
+                if len(al) > 0:
+                    item["url"] = response.urljoin(al[0].css("::attr(href)").get())
             yield item
-            if item["url"]:
-                yield scrapy.Request(url=item["url"], callback=callback, meta={"meta_parent": item})
+            if "url" in item:
+                yield scrapy.Request(url=item["url"], callback=self.parse, meta={"meta_parent": item})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    execute(['scrapy', 'crawl', 'example'])
+    execute(["scrapy", "crawl", "example", "-L", "WARN"])
