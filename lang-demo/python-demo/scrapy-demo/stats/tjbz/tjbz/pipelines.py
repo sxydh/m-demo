af42c49d2cde69a40b76eacf2c26716c5e71ffdf
sxydh <sxydhgg@gmail.com>
2024-09-22 13:36:33 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/stats/tjbz/tjbz/pipelines.py
+++ lang-demo/python-demo/scrapy-demo/stats/tjbz/tjbz/pipelines.py
@@ -5,9 +5,9 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
 
 
 class TjbzPipeline:
     def process_item(self, item, spider):
-        return item
+        print(item)
+        pass
