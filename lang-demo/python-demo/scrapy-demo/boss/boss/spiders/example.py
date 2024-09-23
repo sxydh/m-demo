611c67d984b3d10e6ed927c3f330341613fff9de
sxydh <sxydhgg@gmail.com>
2024-09-24 01:55:32 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/boss/boss/spiders/example.py
+++ lang-demo/python-demo/scrapy-demo/boss/boss/spiders/example.py
@@ -77,4 +77,4 @@ class ExampleSpider(scrapy.Spider):
 
 if __name__ == "__main__":
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    execute(["scrapy", "crawl", "example", "-L", "WARN"])
+    execute(["scrapy", "crawl", "example", "-L", "DEBUG"])
