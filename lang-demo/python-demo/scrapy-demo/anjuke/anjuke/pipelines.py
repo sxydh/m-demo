93a29187614eb1c7ce3abd2aec4fb7784a0a309b
sxydh <sxydhgg@gmail.com>
2024-09-23 16:51:03 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/anjuke/anjuke/pipelines.py
+++ lang-demo/python-demo/scrapy-demo/anjuke/anjuke/pipelines.py
@@ -18,7 +18,7 @@ class AnjukePipeline:
     def open_spider(self, spider):
         self.conn = get_sqlite_connection()
         self.conn.execute("create table if not exists anjuke_city(uid text, province text, name text, url text, new_house_url text, new_house_total text, new_house_total_num integer, body text, remark text)")
-        self.conn.execute("create table if not exists anjuke_new_house(uid text, province text, city text, name text, address text, type text, tag text, price text, price_num integer, url text, body text, remark text)")
+        self.conn.execute("create table if not exists anjuke_new_house(uid text, province text, city text, city_new_house_url text, name text, address text, type text, tag text, price text, price_num integer, url text, body text, remark text)")
         self.conn.execute("delete from anjuke_city where 1 = 1")
         self.conn.execute("delete from anjuke_new_house where 1 = 1")
         self.conn.commit()
@@ -35,11 +35,11 @@ class AnjukePipeline:
             self.conn.commit()
         elif isinstance(item, NewHouseItem):
             if item.get("uid") is None:
-                self.conn.execute("insert into anjuke_new_house(uid, province, city, name, address, type, tag, price, price_num, url, body, remark) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                                  (str(uuid.uuid4()), item.get("province"), item.get("city"), item.get("name"), item.get("address"), item.get("type"), item.get("tag"), item.get("price"), item.get("price_num"), item.get("url"), item.get("body"), item.get("remark")))
+                self.conn.execute("insert into anjuke_new_house(uid, province, city, city_new_house_url, name, address, type, tag, price, price_num, url, body, remark) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                                  (str(uuid.uuid4()), item.get("province"), item.get("city"), item.get("city_new_house_url"), item.get("name"), item.get("address"), item.get("type"), item.get("tag"), item.get("price"), item.get("price_num"), item.get("url"), item.get("body"), item.get("remark")))
             else:
-                self.conn.execute("update anjuke_new_house set province = ?, city = ?, name = ?, address = ?, type = ?, tag = ?, price = ?, price_num = ?, url = ?, body = ?, remark = ? where uid = ?",
-                                  (item.get("province"), item.get("city"), item.get("name"), item.get("address"), item.get("type"), item.get("tag"), item.get("price"), item.get("price_num"), item.get("url"), item.get("body"), item.get("remark"), item.get("uid")))
+                self.conn.execute("update anjuke_new_house set province = ?, city = ?, city_new_house_url = ?, name = ?, address = ?, type = ?, tag = ?, price = ?, price_num = ?, url = ?, body = ?, remark = ? where uid = ?",
+                                  (item.get("province"), item.get("city"), item.get("city_new_house_url"), item.get("name"), item.get("address"), item.get("type"), item.get("tag"), item.get("price"), item.get("price_num"), item.get("url"), item.get("body"), item.get("remark"), item.get("uid")))
             self.conn.commit()
 
     def close_spider(self, spider):
