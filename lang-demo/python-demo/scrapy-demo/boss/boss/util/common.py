be1c3fc0a6de6580e08e63d884ad850e3a3e9b06
sxydh <sxydhgg@gmail.com>
2024-09-24 00:58:08 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/boss/boss/util/common.py
+++ lang-demo/python-demo/scrapy-demo/boss/boss/util/common.py
@@ -2,6 +2,6 @@ import os
 import sqlite3
 
 
-def get_sqlite_connection(f='anjuke.db'):
+def get_sqlite_connection(f='boss.db'):
     os.makedirs('tmp', exist_ok=True)
     return sqlite3.connect(f'tmp/{f}')
