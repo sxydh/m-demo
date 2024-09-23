d4b1385d8d04e3b008641eccde0d58ca8d9e6dfd
sxydh <sxydhgg@gmail.com>
2024-09-24 00:50:54 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/boss/boss/spiders/example.py
+++ lang-demo/python-demo/scrapy-demo/boss/boss/spiders/example.py
@@ -1,10 +1,21 @@
+import os
+import sys
+from typing import Any
+
 import scrapy
+from scrapy.cmdline import execute
+from scrapy.http import Response
 
 
 class ExampleSpider(scrapy.Spider):
     name = "example"
-    allowed_domains = ["zhipin.com"]
+    allowed_domains = []
     start_urls = ["https://zhipin.com"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         pass
+
+
+if __name__ == "__main__":
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    execute(["scrapy", "crawl", "example", "-L", "WARN"])
