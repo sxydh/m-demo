14f020dd866ec7404558ea8f1bbf7382f288216b
sxydh <sxydhgg@gmail.com>
2024-09-24 12:30:05 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/boss/boss/pipelines.py
+++ lang-demo/python-demo/scrapy-demo/boss/boss/pipelines.py
@@ -3,11 +3,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import os
+import threading
 
 from boss.util.common import get_sqlite_connection
 
-
 # useful for handling different item types with a single interface
+
+job_rlock = threading.RLock()
 
 
 class BossPipeline:
@@ -20,9 +22,12 @@ class BossPipeline:
         self.conn.commit()
 
     def process_item(self, item, spider):
-        self.conn.execute("insert into boss_job(name, address, salary, company, city, industry, experience, degree, scale, job_tag, company_tag, body, job_id, job_url, job_list_url, remark) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                          (item.get("name"), item.get("address"), item.get("salary"), item.get("company"), item.get("city"), item.get("industry"), item.get("experience"), item.get("degree"), item.get("scale"), item.get("job_tag"), item.get("company_tag"), item.get("body"), item.get("job_id"), item.get("job_url"), item.get("job_list_url"), item.get("remark")))
-        self.conn.commit()
+        with job_rlock:
+            if self.conn.execute("select 1 from boss_job where job_id = ?", [item.get("job_id")]).fetchone():
+                return
+            self.conn.execute("insert into boss_job(name, address, salary, company, city, industry, experience, degree, scale, job_tag, company_tag, body, job_id, job_url, job_list_url, remark) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                              (item.get("name"), item.get("address"), item.get("salary"), item.get("company"), item.get("city"), item.get("industry"), item.get("experience"), item.get("degree"), item.get("scale"), item.get("job_tag"), item.get("company_tag"), item.get("body"), item.get("job_id"), item.get("job_url"), item.get("job_list_url"), item.get("remark")))
+            self.conn.commit()
 
     def close_spider(self, spider):
         self.conn.close()
