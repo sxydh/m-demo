35523d5e21fc6084f5df58e4e6e9ea04fb94f4b9
sxydh <sxydhgg@gmail.com>
2024-09-25 14:26:18 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/qcwy/_qcwy_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/qcwy/_qcwy_app.py
@@ -3,13 +3,12 @@ from src.main.util.common import get_sqlite_connection
 
 
 class QcwyApp:
-
     start_url = "https://we.51job.com/pc/search?searchType=2&sortType=1"
     job_areas = ["090200"]
     functions = []
-    work_years = []
-    degrees = []
-    company_sizes = []
+    work_years = [("02", "1-3年"), ("03", "3-5年"), ("04", "5-10年"), ("05", "10年以上"), ("06", "无需经验")]
+    degrees = [("01", "初中及以下"), ("02", "高中/中技/中专"), ("03", "大专"), ("04", "本科"), ("05", "硕士"), ("06", "博士"), ("07", "无")]
+    company_sizes = [("01", "少于50人"), ("02", "50-150人"), ("03", "150-500人"), ("04", "500-1000人"), ("05", "1000-5000人"), ("06", "5000-10000人"), ("07", "10000人以上")]
 
     def __init__(self):
         self.cli = Cli()
