429e6370f513d79a15cb7ad8e48ed6746a81da7c
sxydh <sxydhgg@gmail.com>
2024-09-25 16:57:10 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/util/cli.py
+++ lang-demo/python-demo/selenium-demo/src/main/util/cli.py
@@ -89,7 +89,7 @@ class Cli:
         while True:
             if moved_x_offset >= x_offset:
                 break
-            delta_x_offset = random.randint(1, 3)
+            delta_x_offset = min(random.randint(10, 20), x_offset - moved_x_offset)
             action_chains.move_by_offset(delta_x_offset, 0).perform()
             moved_x_offset += delta_x_offset
             sleep(random.uniform(0.05, 0.2))
