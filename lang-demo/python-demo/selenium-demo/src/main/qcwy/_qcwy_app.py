f11fa24e6c757990fff0819da8b5945b77155515
sxydh <sxydhgg@gmail.com>
2024-09-26 06:02:16 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/qcwy/_qcwy_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/qcwy/_qcwy_app.py
@@ -127,6 +127,7 @@ class QcwyApp(threading.Thread):
             if updated.rowcount == 0:
                 continue
 
+            logging.debug(f'running url: {url}')
             self.cli.get(url)
             page = 1
             while True:
