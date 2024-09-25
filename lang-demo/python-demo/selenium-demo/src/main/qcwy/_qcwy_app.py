c2f6adcc403cf7c84a31d02a0a0fa4790c5caaf2
sxydh <sxydhgg@gmail.com>
2024-09-25 14:22:16 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/qcwy/_qcwy_app.py
+++ lang-demo/python-demo/selenium-demo/src/main/qcwy/_qcwy_app.py
@@ -4,6 +4,13 @@ from src.main.util.common import get_sqlite_connection
 
 class QcwyApp:
 
+    start_url = "https://we.51job.com/pc/search?searchType=2&sortType=1"
+    job_areas = ["090200"]
+    functions = []
+    work_years = []
+    degrees = []
+    company_sizes = []
+
     def __init__(self):
         self.cli = Cli()
         self.conn = get_sqlite_connection()
