1f1e0a3bf3c1a32da0d9275d2b6aeb5f0e2e026f
sxydh <sxydhgg@gmail.com>
2024-09-27 23:51:50 +0800

    [UPDATE]

---
+++ lang-demo/python-demo/scrapy-demo/anjuke/anjuke/spiders/geocoder_app.py
@@ -0,0 +1,45 @@
+import os
+import time
+
+from m_pyutil.msqlite import select_one, save
+from m_pyutil.mtencent_map import TencentMap
+
+DB_FILE = "anjuke.db"
+
+
+def geocoder_handler():
+    tencent_map = TencentMap(key=os.environ.get("TENCENT_MAP_KEY"))
+    while True:
+        new_house = select_one(sql="select id, city, address from anjuke_new_house where location is null limit 1",
+                               f=DB_FILE)
+        if not new_house:
+            break
+
+        id = new_house[0]
+        city = new_house[1]
+        address = new_house[2]
+        if not address or len(address) == 0:
+            return
+        address = address.split("]")[1]
+        address = f"{city}市{address}"
+
+        response = tencent_map.geocoder(address=address)
+        if response.status_code != 200:
+            continue
+        result = response.json()
+        status = result.get("status")
+        if status != 0:
+            continue
+        location = result.get("result").get("location")
+        lng = location.get("lng")
+        lat = location.get("lat")
+        location = f"{lng},{lat}"
+
+        save(sql="update anjuke_new_house set location=? where id=?",
+             params=[location, id],
+             f=DB_FILE)
+        time.sleep(0.2)
+
+
+if __name__ == '__main__':
+    geocoder_handler()
