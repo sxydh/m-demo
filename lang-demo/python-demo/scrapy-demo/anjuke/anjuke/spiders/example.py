7deedc822948d53d7e71f710af5fb2f9ad8dd6d2
sxydh <sxydhgg@gmail.com>
2024-09-27 22:38:03 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/anjuke/anjuke/spiders/example.py
+++ lang-demo/python-demo/scrapy-demo/anjuke/anjuke/spiders/example.py
@@ -24,16 +24,9 @@ class ExampleSpider(scrapy.Spider):
     start_urls = ["https://www.anjuke.com/sy-city.html?from=HomePage_City"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        if self.is_need_request_again(self.start_urls[0], response):
+        if self.is_need_request_again(target_url=self.start_urls[0], response=response):
             yield scrapy.Request(self.start_urls[0], callback=self.parse, dont_filter=True)
             return
-
-        global city_rlock
-        global city_handled
-        with city_rlock:
-            if city_handled:
-                return
-            city_handled = True
 
         cities = response.css(".ajk-city-cell.is-letter li a")
         for city in cities:
@@ -48,26 +41,25 @@ class ExampleSpider(scrapy.Spider):
         meta_city_item = response.meta["meta_city_item"]
         url = meta_city_item["url"]
 
-        if self.is_need_request_again(url, response):
+        if self.is_need_request_again(target_url=url, response=response):
             yield scrapy.Request(url, callback=self.parse_city_new_house_url, meta={"meta_city_item": copy.copy(meta_city_item)}, dont_filter=True)
             return
 
         navs = response.css(".nav-channel-list > li:first-child")
         if len(navs) == 0:
             meta_city_item["body"] = response.body
-            meta_city_item["remark"] = "len(response.css(\".nav-channel-list > li:first-child\")) == 0"
+            meta_city_item["remark"] = "len(response.css(\".nav-channel-list > li:first-child\")) == 0###找不到导航栏"
             yield meta_city_item
             return
         nav = navs[0]
         nav_text = nav.css("::text").get().strip()
         if nav_text != "新房":
             meta_city_item["body"] = response.body
-            meta_city_item["remark"] = f"nav.css(\"::text\").get().strip() == \"{nav_text}\""
+            meta_city_item["remark"] = f"nav.css(\"::text\").get().strip() == \"{nav_text}\"###找不到新房导航"
             yield meta_city_item
             return
         new_house_url = response.urljoin(nav.css("a::attr(href)").get())
         new_house_url = new_house_url.split("?")[0]
-
         new_house_url = f"{new_house_url}loupan/all/s1"
         meta_city_item["new_house_url"] = new_house_url
         yield meta_city_item
@@ -78,16 +70,16 @@ class ExampleSpider(scrapy.Spider):
         new_house_url = meta_city_item["new_house_url"]
         city_name = meta_city_item["name"]
 
-        if self.is_need_request_again(new_house_url, response):
+        if self.is_need_request_again(target_url=new_house_url, response=response):
             meta_city_item["body"] = response.body
-            meta_city_item["remark"] = "is_need_request_again"
+            meta_city_item["remark"] = "is_need_request_again###人机检测"
             yield meta_city_item
             yield scrapy.Request(new_house_url, callback=self.parse_new_house_list, meta={"meta_city_item": copy.copy(meta_city_item)}, dont_filter=True)
             return
 
         page_city_name = self.parse_text_helper(response, ".sel-city .city", replaces=[" "])
         if page_city_name is None or (page_city_name != city_name and not (page_city_name in city_name or city_name in page_city_name)):
-            logging.warning(f"### city_name from page ### {page_city_name} <=> {city_name}")
+            logging.warning(f"### page_city_name warning ### page_city_name={page_city_name} <=> city_name={city_name}")
             meta_city_item["body"] = response.body
             meta_city_item["remark"] = f"page_city_name={page_city_name} <=> city_name={city_name}"
             yield meta_city_item
@@ -105,12 +97,12 @@ class ExampleSpider(scrapy.Spider):
         new_house_list = response.css(".list-results .item-mod")
         if len(new_house_list) == 0:
             meta_city_item["body"] = response.body
-            meta_city_item["remark"] = "len(new_house_list) == 0"
+            meta_city_item["remark"] = "len(new_house_list) == 0###新房列表为空"
             yield meta_city_item
             return
         for new_house in new_house_list:
             new_house_item = NewHouseItem()
-            new_house_item["city"] = meta_city_item["name"]
+            new_house_item["city"] = city_name
             new_house_item["city_new_house_url"] = new_house_url
             new_house_item["name"] = self.parse_text_helper(new_house, ".lp-name", replaces=[" "])
             new_house_item["address"] = self.parse_text_helper(new_house, ".address", replaces=[" "])
@@ -124,7 +116,7 @@ class ExampleSpider(scrapy.Spider):
             new_house_item["body"] = str(new_house)
             yield new_house_item
 
-    def parse_text_helper(self, src, selector, is_multi=False, replaces: list = None):
+    def parse_text_helper(self, src: Any, selector: str, is_multi: bool = False, replaces: list = None):
         ret = None
         arr = src.css(selector)
         if len(arr) == 0:
@@ -135,11 +127,8 @@ class ExampleSpider(scrapy.Spider):
             if len(texts) > 0:
                 ret = "".join(texts)
         else:
-            ret = []
-            for ele in arr:
-                text = ele.css("::text").get()
-                if text:
-                    ret.append(text.strip())
+            ret = [ele.css("::text").get() for ele in arr]
+            ret = [ele.strip() for ele in ret if ele]
             ret = "###".join(ret)
         if ret:
             ret = ret.replace("\xa0", "").strip()
@@ -151,18 +140,18 @@ class ExampleSpider(scrapy.Spider):
     def is_need_request_again(self, target_url: str, response: Response = None) -> bool:
         # 页面是反爬验证
         antibot = response.css("#\\@\\@xxzlGatewayUrl")
-        antibot_url = antibot[0].css("::text").get().strip() if antibot is not None and len(antibot) > 0 else None
-        if antibot_url is not None:
-            logging.warning(f"### antibot from page ### {antibot_url} <=> {target_url}")
+        antibot_url = antibot[0].css("::text").get().strip() if antibot and len(antibot) > 0 else None
+        if antibot_url:
+            logging.warning(f"### antibot page ### {antibot_url} <=> {target_url}")
             return True
         # 页面没有内容
         body = response.css("body")
         if len(body) == 0:
-            logging.warning(f"### empty page ### len(body) == 0 <=> {target_url}")
+            logging.warning(f"### empty page ### {target_url}")
             return True
         # 地址是反爬验证
         if "callback" in response.url:
-            logging.warning(f"### antibot from url ### {response.url} <=> {target_url}")
+            logging.warning(f"### antibot url ### {response.url} <=> {target_url}")
             return True
         # 其它错误码
         if response.status != 200:
