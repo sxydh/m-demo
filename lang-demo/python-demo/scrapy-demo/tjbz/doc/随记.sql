cdbfc15e06ecc3cde3ee52be514fadaed96f4e8a
sxydh <sxydhgg@gmail.com>
2024-09-27 23:14:59 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/tjbz/doc/随记.sql
+++ lang-demo/python-demo/scrapy-demo/tjbz/doc/随记.sql
@@ -1,10 +1,10 @@
 -- 查询节点路径
 with recursive path_cte as (
-select name, code, parent_code, code as path
+select name, code, parent_code, code as path, name as province
 from tjbz
 where parent_code is null
 union all
-select t.name, t.code, t.parent_code, p.path || '>' || t.code as path
+select t.name, t.code, t.parent_code, p.path || '>' || t.code as path, p.province
 from tjbz t
 join path_cte p on t.parent_code = p.code)
 select *
