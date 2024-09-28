21a48d7fef69c94d0eedbdb87c7e7f96fc718096
sxydh <sxydhgg@gmail.com>
2024-09-28 17:53:58 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/qcwy/tmp/blockres-demo/injected.js
+++ lang-demo/python-demo/selenium-demo/src/main/qcwy/tmp/blockres-demo/injected.js
@@ -24,7 +24,7 @@
                             })
                         }
                     ).catch(e => {
-                        // TODO NOTHING
+                        // NOTHING
                     });
                 }
             });
