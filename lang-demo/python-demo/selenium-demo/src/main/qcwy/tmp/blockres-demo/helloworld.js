298c5e9070d8e0a7db18567d0d11b47a0e014915
sxydh <sxydhgg@gmail.com>
2024-09-28 17:52:12 +0800

    [UPDATE]

---
+++ lang-demo/python-demo/selenium-demo/src/main/qcwy/tmp/blockres-demo/helloworld.js
@@ -0,0 +1,15 @@
+function helloworld() {
+    alert('Hello World');
+}
+
+setTimeout(() => {
+    document.getElementById('helloworldButton').addEventListener('click', () => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: helloworld,
+            });
+        });
+    });
+}, 500)
+
---
+++ lang-demo/python-demo/selenium-demo/src/main/qcwy/tmp/blockres-demo/injected.js
@@ -0,0 +1,39 @@
+(function () {
+    console.log('ready to override XMLHttpRequest', new Date().toLocaleTimeString());
+
+    const originalXMLHttpRequest = window.XMLHttpRequest;
+
+    function newXMLHttpRequest() {
+        const xhr = new originalXMLHttpRequest();
+
+        const originalOpen = xhr.open;
+        xhr.open = function (method, url) {
+            return originalOpen.apply(xhr, arguments);
+        };
+
+        const originalSend = xhr.send;
+        xhr.send = function (body) {
+            xhr.addEventListener('load', function () {
+                if (xhr.responseType != 'blob') {
+                    fetch('http://localhost:8080',
+                        {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                url: xhr.responseURL,
+                                response: xhr.response
+                            })
+                        }
+                    ).catch(e => {
+                        // TODO NOTHING
+                    });
+                }
+            });
+            return originalSend.apply(xhr, arguments);
+        };
+        return xhr;
+    }
+
+    window.XMLHttpRequest = newXMLHttpRequest;
+
+    console.log('finish to override XMLHttpRequest', new Date().toLocaleTimeString());
+})();
