bc579a8ab1f1ac83c5ee4eb5f10780dbf834c1ce
sxydh <sxydhgg@gmail.com>
2024-09-29 18:56:40 +0800

    [UPDATE]

---
+++ lang-demo/python-demo/selenium-demo/src/main/qcwy/export_mongo.py
@@ -0,0 +1,10 @@
+from m_pyutil.mmongo import MongoCli
+
+if __name__ == '__main__':
+    mongo_cli = MongoCli(host='192.168.233.129',
+                         database='qcwy')
+    mongo_cli.import_sqlite(f='qcwy.db',
+                            table='qcwy_job',
+                            sql='select raw from qcwy_job order by id',
+                            collection='qcwy_job')
+    mongo_cli.close()
