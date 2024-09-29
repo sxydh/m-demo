9917225e50ba1ef5666b435c4215777fd319e850
sxydh <sxydhgg@gmail.com>
2024-09-29 19:37:33 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/qcwy/export_mongo.py
+++ lang-demo/python-demo/selenium-demo/src/main/qcwy/export_mongo.py
@@ -1,6 +1,11 @@
 from m_pyutil.mmongo import MongoCli
 
 if __name__ == '__main__':
+    # 需要额外执行以下脚本
+    # db.qcwy_job.find({}).forEach(e => {
+    #     db.qcwy_job.updateOne({ _id: e._id }, { $set: { raw: JSON.parse(e.raw) } });
+    # });
+
     mongo_cli = MongoCli(host='192.168.233.129',
                          database='qcwy')
     mongo_cli.import_sqlite(f='qcwy.db',
