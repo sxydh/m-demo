56357342f2a5ac4605823198a9a8f723b0d561a0
sxydh <sxydhgg@gmail.com>
2024-09-30 23:11:53 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/src/main/qcwy/tmp/blockres-demo/inject.js
+++
@@ -1,7 +0,0 @@
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('injected.js');
-s.onload = function () {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
-console.log('have injected "injected.js"', new Date().toLocaleTimeString());
