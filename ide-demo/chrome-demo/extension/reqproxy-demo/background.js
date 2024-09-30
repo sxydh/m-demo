e442c82cc935ff5ea25653db42761aac3fd70426
sxydh <sxydhgg@gmail.com>
2024-09-30 22:56:54 +0800

    [UPDATE]

--- ide-demo/chrome-demo/extension/reqproxy-demo/background.js
+++ ide-demo/chrome-demo/extension/reqproxy-demo/background.js
@@ -1,21 +1,39 @@
-// https://developer.chrome.com/docs/extensions/reference/api/proxy?hl=zh-cn#description
-
-const config = {
-    mode: "fixed_servers",
-    rules: {
-        singleProxy: {
-            scheme: "http",
-            host: "127.0.0.1",
-            port: 10809
-        },
-        bypassList: ["localhost"]
-    }
-};
-
-chrome.proxy.settings.set(
-    {
-        value: config,
-        scope: 'regular'
-    },
-    function () { }
-);
+setInterval(() => {
+    fetch('http://localhost:8080/proxy_config')
+        .then(res => {
+            if (!res.ok) {
+                console.debug(res.url, res.status, res.statusText);
+                return Promise.reject();
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.debug('proxy_config', data);
+            let host = data.host;
+            let port = data.port;
+            if (host && port) {
+                // https://developer.chrome.com/docs/extensions/reference/api/proxy?hl=zh-cn#description
+                const config = {
+                    mode: 'fixed_servers',
+                    rules: {
+                        singleProxy: {
+                            scheme: 'http',
+                            host: host,
+                            port: port
+                        },
+                        bypassList: ['localhost']
+                    }
+                };
+                chrome.proxy.settings.set(
+                    {
+                        value: config,
+                        scope: 'regular'
+                    },
+                    function () { }
+                );
+            }
+        })
+        .catch(() => {
+            // NOTHING
+        });
+}, 2000);
