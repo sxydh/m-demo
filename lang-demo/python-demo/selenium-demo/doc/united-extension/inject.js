0ca36e6b7582385208407b45adf6ba67d2d0698f
sxydh <sxydhgg@gmail.com>
2024-09-30 23:14:24 +0800

    [UPDATE]

---
+++ lang-demo/python-demo/selenium-demo/doc/united-extension/inject.js
@@ -0,0 +1,7 @@
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('injected.js');
+s.onload = function () {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
+console.log('have injected "injected.js"', new Date().toLocaleTimeString());
---
+++ lang-demo/python-demo/selenium-demo/doc/united-extension/injected.js
@@ -0,0 +1,40 @@
+(function () {
+    console.log('ready to override XMLHttpRequest', new Date().toLocaleTimeString());
+
+    const originalXMLHttpRequest = window.XMLHttpRequest;
+
+    function newXMLHttpRequest() {
+        const xhr = new originalXMLHttpRequest();
+
+        const originalOpen = xhr.open;
+        xhr.open = function (method, url) {
+            return originalOpen.apply(xhr, arguments);
+        };
+
+        const originalSend = xhr.send;
+        xhr.send = function (body) {
+            xhr.addEventListener('load', function () {
+                if (xhr.responseType !== 'blob') {
+                    let isFiltered = !xhr.responseURL.includes('https://we.51job.com/api/job/search-pc')
+                    isFiltered = isFiltered || !xhr.responseURL.includes('&function=')
+                    if (!isFiltered) {
+                        fetch(`http://localhost:8080/?url=${encodeURIComponent(xhr.responseURL)}`,
+                            {
+                                method: 'POST',
+                                body: xhr.response
+                            }
+                        ).catch(() => {
+                            // NOTHING
+                        });
+                    }
+                }
+            });
+            return originalSend.apply(xhr, arguments);
+        };
+        return xhr;
+    }
+
+    window.XMLHttpRequest = newXMLHttpRequest;
+
+    console.log('finish to override XMLHttpRequest', new Date().toLocaleTimeString());
+})();
