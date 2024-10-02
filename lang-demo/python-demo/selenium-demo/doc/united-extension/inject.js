c4c61e266de0b8b9e3535d9c60efb62f39121575
sxydh <sxydhgg@gmail.com>
2024-10-02 20:14:03 +0800

    [UPDATE]

--- lang-demo/python-demo/selenium-demo/doc/united-extension/inject.js
+++ lang-demo/python-demo/selenium-demo/doc/united-extension/inject.js
@@ -5,3 +5,9 @@ s.onload = function () {
 };
 (document.head || document.documentElement).appendChild(s);
 console.debug('have injected "injected.js"', new Date().toLocaleTimeString());
+
+// 前程无忧
+let interval = setInterval(() => {
+    console.debug('interval', interval);
+    document.querySelectorAll('.joblist-item .chat').forEach(e => e.remove());
+}, 1000);
