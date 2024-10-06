e56f8e9028a36737a7a79e2f6a27162c00660868
sxydh <sxydhgg@gmail.com>
2024-10-06 15:57:54 +0800

    [UPDATE]

--- lang-demo/python-demo/scrapy-demo/tjbz/tjbz/spiders/example.py
+++ lang-demo/python-demo/scrapy-demo/tjbz/tjbz/spiders/example.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import sys
 from typing import Any
@@ -9,7 +10,6 @@ from scrapy.http import Response
 from tjbz.items import TjbzItem
 
 
-# noinspection DuplicatedCode
 class ExampleSpider(scrapy.Spider):
     name = "example"
     allowed_domains = ["stats.gov.cn"]
@@ -17,51 +17,50 @@ class ExampleSpider(scrapy.Spider):
     max_level = 4
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        meta = response.meta
+        meta: dict = response.meta
         meta_parent = meta.get("meta_parent", TjbzItem())
-        parent_code = meta_parent.get("code", None)
+        parent_code = meta_parent.get("code")
 
         trs = response.css(".provincetr td")
         level = 1
-        if len(trs) == 0 and self.max_level > 1:
-            trs += response.css(".citytr")
+        if len(trs) != 0:
+            pass
+        elif self.max_level > 1:
+            trs = response.css(".citytr")
             level = 2
-        if len(trs) == 0 and self.max_level > 2:
-            trs += response.css(".countytr")
+        elif self.max_level > 2:
+            trs = response.css(".countytr")
             level = 3
-        if len(trs) == 0 and self.max_level > 3:
-            trs += response.css(".towntr")
+        elif self.max_level > 3:
+            trs = response.css(".towntr")
             level = 4
 
-        if len(trs) == 0:
-            if "url" in meta_parent:
-                yield scrapy.Request(url=meta_parent["url"], callback=self.parse, meta={"meta_parent": meta_parent}, dont_filter=True)
-                return
+        # 页面异常重试
+        if len(trs) == 0 and meta_parent.get("url"):
+            logging.warning(f"none data page: {meta_parent.get('url')} <=> {response.url}")
+            yield scrapy.Request(url=meta_parent.get("url"), callback=self.parse, meta={"meta_parent": meta_parent}, dont_filter=True)
+            return
 
         for tr in trs:
             tds = tr.css("td")
-            item = TjbzItem()
             if len(tds) == 1:
-                al = tds[0].css("a")
-                href = al[0].css("::attr(href)").get()
-                item["code"] = href.replace('.html', '')
-                item["name"] = tds[0].css("::text").get().strip()
-                item["url"] = response.urljoin(href)
-                item["level"] = level
-            else:
-                item["code"] = tds[0].css("::text").get().strip()
-                item["name"] = tds[-1].css("::text").get().strip()
-                item["parent_code"] = parent_code
-                al = tds[0].css("a")
-                if len(al) > 0:
-                    item["url"] = response.urljoin(al[0].css("::attr(href)").get())
-                item["level"] = level
+                logging.warning(f"wrong data page: {meta_parent.get('url')}")
+                continue
+            item = TjbzItem()
+            item["code"] = tds[0].css("::text").get().strip()
+            item["name"] = tds[-1].css("::text").get().strip()
+            item["parent_code"] = parent_code
+            item["level"] = level
+            alist = tds[0].css("a")
+            if len(alist) > 0:
+                item["url"] = response.urljoin(alist[0].css("::attr(href)").get())
             yield item
 
-            if "url" in item and item["level"] < self.max_level:
-                yield scrapy.Request(url=item["url"], callback=self.parse, meta={"meta_parent": item})
+            if item.get("url") and level < self.max_level:
+                yield scrapy.Request(url=item.get("url"), callback=self.parse, meta={"meta_parent": item})
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.WARN)
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     execute(["scrapy", "crawl", "example", "-L", "DEBUG"])
