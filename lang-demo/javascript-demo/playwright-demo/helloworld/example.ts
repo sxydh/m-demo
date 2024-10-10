9d1795cf8d78f82328615c17a47bebc371c2d855
sxydh <sxydhgg@gmail.com>
2024-10-10 14:20:20 +0800

    [UPDATE]

--- lang-demo/javascript-demo/playwright-demo/helloworld/example.ts
+++ lang-demo/javascript-demo/playwright-demo/helloworld/example.ts
@@ -1,7 +1,7 @@
 import { chromium } from 'playwright';
 
 (async () => {
-    const browser = await chromium.launch();
+    const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
 
