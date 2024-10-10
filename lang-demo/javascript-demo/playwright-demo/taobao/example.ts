4e04a53676822645e0bb7f6fb64343b4e9475eaf
sxydh <sxydhgg@gmail.com>
2024-10-10 14:59:24 +0800

    [UPDATE]

--- lang-demo/javascript-demo/playwright-demo/taobao/example.ts
+++ lang-demo/javascript-demo/playwright-demo/taobao/example.ts
@@ -7,7 +7,8 @@ import { chromium } from 'playwright';
     });
     const context = await browser.newContext();
     // https://github.com/berstend/puppeteer-extra/blob/master/packages/extract-stealth-evasions/readme.md
-    // npx extract-stealth-evasions 生成文件 stealth.min.js
+    // 生成文件命令： npx extract-stealth-evasions
+    // 检测机器人网址： https://www.browserscan.net/bot-detection
     context.addInitScript({ path: './stealth.min.js' });
     const page = await context.newPage();
 
