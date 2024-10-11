8d0e4378ba73cbc364dc2da8247ef19c78223dd4
sxydh <sxydhgg@gmail.com>
2024-10-11 13:10:05 +0800

    [UPDATE]

--- lang-demo/javascript-demo/puppeteer-demo/helloword/example.ts
+++ lang-demo/javascript-demo/puppeteer-demo/helloword/example.ts
@@ -1,7 +1,11 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        args: ["--start-maximized"]
+    });
     const page = await browser.newPage();
     await page.goto('https://www.baidu.com');
 })();
