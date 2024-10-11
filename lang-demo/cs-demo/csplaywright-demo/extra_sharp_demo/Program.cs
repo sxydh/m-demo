c604278e391026d35cbe5ab64e4528859753657b
sxydh <sxydhgg@gmail.com>
2024-10-11 21:39:57 +0800

    [UPDATE]

--- lang-demo/cs-demo/csplaywright-demo/extra_sharp_demo/Program.cs
+++ lang-demo/cs-demo/csplaywright-demo/extra_sharp_demo/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace extra_sharp_demo
+﻿using PlaywrightExtraSharp.Models;
+using PlaywrightExtraSharp.Plugins.ExtraStealth;
+using PlaywrightExtraSharp;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace extra_sharp_demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Task.Run(async () =>
+            {
+                // https://www.nuget.org/packages/PlaywrightExtraSharp
+                using (var playwrightExtra = new PlaywrightExtra(BrowserTypeEnum.Chromium))
+                {
+                    await playwrightExtra
+                    .Install()
+                    .Use(new StealthExtraPlugin())
+                    .LaunchAsync(new BrowserTypeLaunchOptions()
+                    {
+                        Headless = false
+                    });
+                    var page = await playwrightExtra.NewPageAsync(options: null);
 
+                    await page.GotoAsync("https://www.browserscan.net/bot-detection");
+
+                    await Task.Delay(10000);
+                }
+            }).Wait();
         }
     }
 }
