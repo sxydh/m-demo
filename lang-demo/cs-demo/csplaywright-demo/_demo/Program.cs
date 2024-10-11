d9dc08eb40254fb7c18dad0cdd0cc9184d0334cc
sxydh <sxydhgg@gmail.com>
2024-10-11 19:42:54 +0800

    [UPDATE]

--- lang-demo/cs-demo/csplaywright-demo/_demo/Program.cs
+++ lang-demo/cs-demo/csplaywright-demo/_demo/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace _demo
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace _demo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        async static void Main(string[] args)
         {
+            using (var playwright = await Playwright.CreateAsync())
+            {
+                var launchOptions = new BrowserTypeLaunchOptions
+                {
+                    Headless = false
+                };
+                var browser = await playwright.Chromium.LaunchAsync(launchOptions);
+                var page = await browser.NewPageAsync();
+
+                await page.GotoAsync("https://www.baidu.com");
+
+                await Task.Delay(5000);
+                await browser.CloseAsync();
+            }
         }
     }
 }
