1d9c74dc7baa428de97a701e4fab31cb2347ecbc
sxydh <sxydhgg@gmail.com>
2024-10-17 17:39:49 +0800

    [UPDATE]

--- lang-demo/cs-demo/csquickstart-demo/delegate_demo/_demo.cs
+++ lang-demo/cs-demo/csquickstart-demo/delegate_demo/_demo.cs
@@ -1,9 +1,29 @@
-﻿namespace delegate_demo
+﻿using System;
+
+namespace delegate_demo
 {
     public class _demo
     {
+        /* 委托定义 */
+        private delegate string GetMsg();
+
+        /* 委托变量 */
+        private readonly GetMsg _getMsg;
+
+        private _demo(GetMsg getMsg)
+        {
+            _getMsg = getMsg;
+        }
+
+        private void SayHello()
+        {
+            Console.WriteLine(_getMsg());
+        }
+
         public static void Main(string[] args)
         {
+            var demo = new _demo(() => "Hello World");
+            demo.SayHello();
         }
     }
 }
