cd2c4935a88d7ddee7d286a3ee00d6b0df85ef47
sxydh <sxydhgg@gmail.com>
2024-10-19 14:39:51 +0800

    [UPDATE]

--- lang-demo/python-demo/uiautomator2-demo/tests/_quickstart/test_quickstart_app.py
+++ lang-demo/python-demo/uiautomator2-demo/tests/_quickstart/test_quickstart_app.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from _quickstart.quickstart_app import App, Session, Gesture, Selector
+from _quickstart.common import App, Session, Gesture, Selector
 
 
 class TestApp(TestCase):
