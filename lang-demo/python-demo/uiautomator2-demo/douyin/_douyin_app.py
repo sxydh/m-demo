ee7ee229560b0552f7572e0d3f63f3ae9b506f48
sxydh <sxydhgg@gmail.com>
2024-10-19 14:44:07 +0800

    [UPDATE]

--- lang-demo/python-demo/uiautomator2-demo/douyin/_douyin_app.py
+++ lang-demo/python-demo/uiautomator2-demo/douyin/_douyin_app.py
@@ -1,1 +1,15 @@
+import uiautomator2 as u2
+
+
+class DouYinApp:
+
+    def __init__(self):
+        self.pkg = ""
+        self.d = u2.connect()
+
+    def start(self):
+        self.d.app_start(self.pkg)
+
+
 if __name__ == '__main__':
+    DouYinApp()
