c64bc032bddc3d7b051d84663c24f9b112696275
sxydh <sxydhgg@gmail.com>
2024-10-19 13:01:43 +0800

    [UPDATE]

---
+++ lang-demo/python-demo/uiautomator2-demo/common.py
@@ -0,0 +1,16 @@
+import uiautomator2 as u2
+
+
+class Common:
+
+    def __init__(self):
+        self.d = u2.connect()
+
+    def current(self):
+        """
+        获取当前包名
+
+        :return:
+        """
+
+        print(self.d.app_current())
