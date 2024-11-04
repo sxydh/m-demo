47d3bc8f25fa0125cdec144e0cf03fe2393f619f
sxydh <sxydhgg@gmail.com>
2024-11-04 11:53:18 +0800

    [UPDATE]

--- lang-demo/python-demo/frida-demo/demo.js
+++ lang-demo/python-demo/frida-demo/demo.js
@@ -1,11 +1,12 @@
 setImmediate(function () {
     Java.perform(function () {
-        var targetClass = "java.lang.String";
-        var methodName = "toString";
+        let targetClass = "java.lang.String";
+        let methodName = "toString";
         Java.use(targetClass)[methodName].implementation = function () {
             // 注意高频调用可能会导致奔溃
-            console.log(`toString: ${this.toString()}`);
-            return this.toString();
+            let str = this.toString();
+            console.log(`toString: ${str}`);
+            return str;
         };
         console.log("ready to go");
     });
