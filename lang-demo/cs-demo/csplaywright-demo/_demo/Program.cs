98fbef2c3c24399d663cbc9e2851dbd4f9e32d50
sxydh <sxydhgg@gmail.com>
2024-11-09 18:10:15 +0800

    [UPDATE]

--- lang-demo/cs-demo/csplaywright-demo/_demo/Program.cs
+++ lang-demo/cs-demo/csplaywright-demo/_demo/Program.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Playwright;
-using System.Threading;
+﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Playwright;
 
 namespace _demo
 {
@@ -20,6 +20,34 @@ namespace _demo
                     var browser = await playwright.Chromium.LaunchAsync(launchOptions);
                     var page = await browser.NewPageAsync();
 
+                    /* 生命周期回调 */
+                    // 从上到下优先顺序
+                    page.Request += (_, irequest) =>
+                    {
+                        var type = irequest.GetType();
+                        Console.WriteLine($"Request: {type}");
+                    };
+                    page.Response += (_, iresponse) =>
+                    {
+                        var type = iresponse.GetType();
+                        Console.WriteLine($"Response: {type}");
+                    };
+                    page.DOMContentLoaded += (_, ipage) =>
+                    {
+                        var type = ipage.GetType();
+                        Console.WriteLine($"DOMContentLoaded: {type}");
+                    };
+                    page.Load += (_, ipage) =>
+                    {
+                        var type = ipage.GetType();
+                        Console.WriteLine($"Load: {type}");
+                    };
+                    page.Close += (_, ipage) =>
+                    {
+                        var type = ipage.GetType();
+                        Console.WriteLine($"Close: {type}");
+                    };
+
                     await page.GotoAsync("https://www.baidu.com");
 
                     await Task.Delay(5000);
@@ -27,6 +55,5 @@ namespace _demo
                 }
             }).Wait();
         }
-
     }
 }
