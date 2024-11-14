900b125e616e9039253f8051248a9828c782a113
sxydh <sxydhgg@gmail.com>
2024-11-14 13:35:08 +0800

    [UPDATE]

--- ide-demo/chrome-demo/extension/manifest_v3/common-demo/document_end.js
+++ ide-demo/chrome-demo/extension/manifest_v3/common-demo/document_end.js
@@ -1,19 +1,34 @@
 /* CSDN */
 if (location.href.includes(".csdn.")) {
-    /* 去除关注 */
-    /* 可以选择 */
-    let hide = document.querySelector(".hide-article-box.hide-article-pos.text-center");
-    if (hide) {
-        hide.remove();
-    }
     let style = document.createElement("style");
     style.innerHTML = `
-        #article_content {
-            overflow: auto !important;
+        /* 左侧广告 */
+        aside > .box-shadow {
+            display: none !important;
+        }
+    
+        /* 屏蔽登录 */
+        .passport-login-container, .passport-login-tip-container {
+            display: none !important;
+        }
+        .signin {
+            display: none !important;
         }
         
-        p, pre, pre * {
+        /* 可以选中 */
+        p, pre * {
             user-select: text !important;
+        }
+        #articleSearchTip {
+            display: none !important;
+        }
+            
+        /* 屏蔽关注 */
+        .hide-article-box.hide-article-pos.text-center {
+            display: none !important;
+        }
+        #article_content {
+            overflow: auto !important;
         }`;
     document.head.appendChild(style);
 
@@ -21,15 +36,13 @@ if (location.href.includes(".csdn.")) {
     window.addEventListener("copy", event => {
         event.stopImmediatePropagation();
     }, true);
-    let signins = document.querySelectorAll(".signin");
-    signins.forEach(e => e.remove());
 }
 
 /* 才士题库 */
 if (location.href.includes(".caishi.")) {
     let style = document.createElement("style");
     style.innerHTML = `
-        * {
+        p {
             cursor: unset !important;
             user-select: unset !important;
         }`;
