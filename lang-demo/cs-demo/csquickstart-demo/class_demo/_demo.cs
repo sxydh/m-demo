470196eb05892cc04e09c57c601e3b0813d275ad
sxydh <sxydhgg@gmail.com>
2024-11-15 18:37:32 +0800

    [UPDATE]

--- lang-demo/cs-demo/csquickstart-demo/class_demo/_demo.cs
+++ lang-demo/cs-demo/csquickstart-demo/class_demo/_demo.cs
@@ -1,7 +1,8 @@
-﻿/************ 类 ************/
-using System;
+﻿using System;
 
-namespace ClassDemo
+#pragma warning disable CS0414 // Field is assigned but its value is never used
+
+namespace class_demo
 {
     /************ 类定义 ************/
     // 类是引用类型，存储在堆上。
@@ -12,12 +13,15 @@ namespace ClassDemo
         private int varInt = 1;
         private string varString = "Hello World!";
         public long VarLong { get; set; }
+
         // 静态成员变量
         public static int staticInt = 1;
 
         /************ 构造函数 ************/
         // 无参构造函数
-        public ClassDemo() { }
+        public ClassDemo()
+        {
+        }
 
         // 有参构造函数
         public ClassDemo(int argInt, string argString)
@@ -62,23 +66,25 @@ namespace ClassDemo
         // 静态成员方法
         public static void StaticMethodDemo()
         {
-            System.Console.WriteLine("ClassDemo.StaticMethodDemo 被调用");
+            Console.WriteLine("ClassDemo.StaticMethodDemo 被调用");
         }
 
         /************ 运算符重载 ************/
         // 重载加法
         public static ClassDemo operator +(ClassDemo a, ClassDemo b)
         {
-            ClassDemo classDemo = new ClassDemo();
-            classDemo.varInt = a.varInt + b.varInt;
-            classDemo.varString = a.varString + b.varString;
+            ClassDemo classDemo = new ClassDemo
+            {
+                varInt = a.varInt + b.varInt,
+                varString = a.varString + b.varString
+            };
             return classDemo;
         }
 
         /************ 析构函数 ************/
         ~ClassDemo()
         {
-            System.Console.WriteLine("析构函数 ~ClassDemo 被调用");
+            Console.WriteLine("析构函数 ~ClassDemo 被调用");
         }
     }
 
@@ -90,35 +96,38 @@ namespace ClassDemo
             ClassDemo classDemo = new ClassDemo();
             classDemo.SetVarInt(1);
             classDemo.SetVarString("Hello World!");
-            classDemo.FuncDemo(str => str += 2); // Lambda 表达式
-            classDemo.ActionDemo(str => str += 2);
-            System.Console.WriteLine("ClassDemo.varInt = " + classDemo.GetVarInt());
-            System.Console.WriteLine("ClassDemo.varString = " + classDemo.GetVarString());
+            classDemo.FuncDemo(str => str + 2); // Lambda 表达式
+            classDemo.ActionDemo(Console.WriteLine);
+            Console.WriteLine("ClassDemo.varInt = " + classDemo.GetVarInt());
+            Console.WriteLine("ClassDemo.varString = " + classDemo.GetVarString());
         }
+
         /************ 类静态成员访问 ************/
         void ClassStaticAccessDemo()
         {
             ClassDemo.StaticMethodDemo();
         }
+
         /************ 重载运算符访问 ************/
         void OperatorPlusDemo()
         {
             ClassDemo a = new ClassDemo(1, "Hello World!");
             ClassDemo b = new ClassDemo(1, "Hello World!");
             ClassDemo c = a + b;
-            System.Console.WriteLine("a+b=c varInt = " + c.GetVarInt());
-            System.Console.WriteLine("a+b=c varString = " + c.GetVarString());
+            Console.WriteLine("a+b=c varInt = " + c.GetVarInt());
+            Console.WriteLine("a+b=c varString = " + c.GetVarString());
         }
-        static void Main(string[] args)
+
+        static void Main(string[] _)
         {
             Demo demo = new Demo();
-            System.Console.WriteLine("\n///////////// ClassAccessDemo");
+            Console.WriteLine("\n///////////// ClassAccessDemo");
             demo.ClassAccessDemo();
 
-            System.Console.WriteLine("\n///////////// ClassStaticAccessDemo");
+            Console.WriteLine("\n///////////// ClassStaticAccessDemo");
             demo.ClassStaticAccessDemo();
 
-            System.Console.WriteLine("\n///////////// OperatorPlusDemo");
+            Console.WriteLine("\n///////////// OperatorPlusDemo");
             demo.OperatorPlusDemo();
         }
     }
