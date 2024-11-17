94d787e357ea8570054dd302d982e058a145deb0
sxydh <sxydhgg@gmail.com>
2024-11-17 15:10:28 +0800

    [UPDATE]

---
+++ lang-demo/javascript-demo/playwright-demo/quickstart/example.ts
@@ -0,0 +1,13 @@
+import { chromium } from 'playwright';
+
+(async () => {
+    const browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto('https://www.baidu.com');
+
+    await page.waitForTimeout(5000);
+
+    await browser.close();
+})();
