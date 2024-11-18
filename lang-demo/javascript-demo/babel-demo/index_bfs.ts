aa56bbc924c31ed0ee839ac5030a431864df8c98
sxydh <sxydhgg@gmail.com>
2024-11-18 22:26:18 +0800

    [UPDATE]

---
+++ lang-demo/javascript-demo/babel-demo/index_bfs.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as parser from '@babel/parser';
+import generate from '@babel/generator';
+import * as types from "@babel/types";
+
+const filePath = path.join(__dirname, 'src', 'todo.js');
+const todoJs: string = fs.readFileSync(filePath, 'utf8');
+
+const logAst = types.expressionStatement(
+    types.callExpression(
+        types.memberExpression(types.identifier('console'), types.identifier('log')),
+        [types.numericLiteral(1)]
+    ));
+
+const ast = parser.parse(todoJs, {sourceType: 'module'});
+const stack: [any] = [ast];
+while (stack.length > 0) {
+    const top = stack.pop();
+    if (top.type === 'VariableDeclaration') {
+        const parent: [any] = top._tnerap;
+        parent.splice(parent.indexOf(top), 0, logAst);
+    }
+
+    for (const key in top) {
+        if (key === '_tnerap') continue;
+
+        const value = top[key];
+        if (value instanceof Object && value.hasOwnProperty('type')) {
+            stack.push(value);
+        } else if (value instanceof Array) {
+            for (let i = value.length - 1; i >= 0; i--) {
+                const ele = value[i];
+                ele._tnerap = value;
+                stack.push(ele);
+            }
+        }
+    }
+}
+
+const output = generate(ast);
+console.log(output.code);
