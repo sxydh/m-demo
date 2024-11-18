77c2811e776957af9ca8a7d6f07f04468db7e552
sxydh <sxydhgg@gmail.com>
2024-11-18 23:30:30 +0800

    [UPDATE]

--- lang-demo/javascript-demo/babel-demo/index_bfs.ts
+++ lang-demo/javascript-demo/babel-demo/index_bfs.ts
@@ -4,29 +4,52 @@ import * as parser from '@babel/parser';
 import generate from '@babel/generator';
 import * as types from "@babel/types";
 
+const injectVariableDeclaration = (node: any) => {
+    if (node && node.type === 'VariableDeclaration') {
+        const declarations = node.declarations;
+        if (declarations && declarations.length === 1) {
+            const parent: [any] = node._tnerap;
+            if (parent instanceof Array) {
+                const logAst = types.expressionStatement(
+                    types.callExpression(
+                        types.memberExpression(types.identifier('console'), types.identifier('log')),
+                        [declarations[0].id]
+                    ));
+                parent.splice(parent.indexOf(node) + 1, 0, logAst);
+            }
+        }
+    }
+};
+
+const injectAssignmentExpression = (node: any) => {
+    if (node && node.type === 'ExpressionStatement') {
+        const expression = node.expression;
+        if (expression && expression.type === 'AssignmentExpression') {
+            const parent: [any] = node._tnerap;
+            if (parent instanceof Array) {
+                const logAst = types.expressionStatement(
+                    types.callExpression(
+                        types.memberExpression(types.identifier('console'), types.identifier('log')),
+                        [expression.left]
+                    ));
+                parent.splice(parent.indexOf(node) + 1, 0, logAst);
+            }
+        }
+    }
+};
+
 const src = path.join(__dirname, 'src');
 const todoJs: string = fs.readFileSync(path.join(src, 'todo.js'), 'utf8');
-
-const logAst = types.expressionStatement(
-    types.callExpression(
-        types.memberExpression(types.identifier('console'), types.identifier('log')),
-        [types.numericLiteral(1)]
-    ));
 
 const ast = parser.parse(todoJs, {sourceType: 'module'});
 const stack: [any] = [ast];
 while (stack.length > 0) {
     const top = stack.pop();
-    if (top.type === 'VariableDeclaration') {
-        const parent: [any] = top._tnerap;
-        if (parent instanceof Array) {
-            parent.splice(parent.indexOf(top), 0, logAst);
-        }
-    }
+    injectVariableDeclaration(top);
+    injectAssignmentExpression(top);
 
     for (const key in top) {
         if (key === '_tnerap') continue;
-
         const value = top[key];
         if (value instanceof Object && value.hasOwnProperty('type')) {
             stack.push(value);
