b7156cd0a8be6a01d387eecb6f6d088b6814d2f4
sxydh <sxydhgg@gmail.com>
2024-11-18 17:03:09 +0800

    [UPDATE]

---
+++ lang-demo/javascript-demo/babel-demo/index.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as parser from '@babel/parser';
+import traverse from '@babel/traverse';
+import * as types from '@babel/types';
+import generate from '@babel/generator';
+
+const filePath = path.join(__dirname, 'src', 'todo.js');
+const todoJs: string = fs.readFileSync(filePath, 'utf8');
+
+// https://babeljs.io/docs/babel-parser
+const ast = parser.parse(todoJs);
+
+// https://babeljs.io/docs/babel-traverse
+traverse(ast, {
+    AssignmentExpression(path) {
+        // https://babeljs.io/docs/babel-types
+        const semicolon = types.stringLiteral(';');
+        path.insertBefore(semicolon);
+    }
+});
+
+// https://babeljs.io/docs/babel-generator
+const output = generate(ast);
+console.log(output.code);
