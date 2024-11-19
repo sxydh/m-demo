859ca0d871faee8ce031679dc8f2d15288064d62
sxydh <sxydhgg@gmail.com>
2024-11-19 22:07:05 +0800

    [UPDATE]

--- lang-demo/javascript-demo/babel-demo/src/todo.js
+++ lang-demo/javascript-demo/babel-demo/src/todo.js
@@ -1,13 +1,11 @@
+// noinspection JSUnusedGlobalSymbols,JSUnusedAssignment
+
 let a = 'a';
 let b = 'b', b2 = 'b2';
 let c = c2 = c3 = 'c';
 let [d, d2, [d3, d4]] = ['d', 'd2', ['d3', 'd4']];
 let {e, e2, e3: {e4, e5}} = {e: 'e', e2: 'e2', e3: {e4: 'e4', e5: 'e5'}};
 
-console.log(
-    a,
-    b, b2,
-    c, c2, c3,
-    d, d2,
-    d3, d4,
-    e, e2, e4, e5);
+a = 'a';
+c = c2 = c3 = 'c';
+[d, d2, [d3, d4]] = ['d', 'd2', ['d3', 'd4']];
