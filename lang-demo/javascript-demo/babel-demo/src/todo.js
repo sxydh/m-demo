715d749dbb99f95d8a5969b6aa0cedc611f1476f
sxydh <sxydhgg@gmail.com>
2024-11-19 19:39:59 +0800

    [UPDATE]

--- lang-demo/javascript-demo/babel-demo/src/todo.js
+++ lang-demo/javascript-demo/babel-demo/src/todo.js
@@ -1,2 +1,7 @@
-let str = 'Hello World';
-console.log(str);
+let a = 1;
+let b = 2, c = 3;
+let d = e = 4;
+let [f, g] = [6, 7];
+let {h, i} = {h: 8, i: 9};
+
+console.log(a, b, c, d, e, f, g, h, i);
