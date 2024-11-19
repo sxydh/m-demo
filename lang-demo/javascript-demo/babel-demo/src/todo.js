3ce9f4c0f9a0662ccca5796776fa9e0346cdf1a6
sxydh <sxydhgg@gmail.com>
2024-11-19 19:54:46 +0800

    [UPDATE]

--- lang-demo/javascript-demo/babel-demo/src/todo.js
+++ lang-demo/javascript-demo/babel-demo/src/todo.js
@@ -1,13 +1,13 @@
 let a = 'a';
 let b = 'b', b2 = 'b2';
-let c = c2 = 'c';
+let c = c2 = c3 = 'c';
 let [d, d2, [d3, d4]] = ['d', 'd2', ['d3', 'd4']];
 let {e, e2, e3: {e4, e5}} = {e: 'e', e2: 'e2', e3: {e4: 'e4', e5: 'e5'}};
 
 console.log(
     a,
     b, b2,
-    c, c2,
+    c, c2, c3,
     d, d2,
     d3, d4,
     e, e2, e4, e5);
