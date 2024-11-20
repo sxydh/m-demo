8e09ee9f7b25dc06a7c72a8d6960c0c7f2e51425
sxydh <sxydhgg@gmail.com>
2024-11-20 13:49:47 +0800

    [UPDATE]

---
+++ lang-demo/javascript-demo/puppeteer-demo/quickstart/example.ts
@@ -0,0 +1,25 @@
+import puppeteer, {HTTPRequest, HTTPResponse} from 'puppeteer';
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        args: ["--start-maximized"]
+    });
+    const page = await browser.newPage();
+
+    page.on('request', (req: HTTPRequest) => {
+        console.log(`request: ${req.url().substring(0, 50)}...`);
+    });
+    page.on('response', async (res: HTTPResponse) => {
+        console.log(`response: ${res.headers()['content-type']}`);
+    });
+    page.on('domcontentloaded', () => {
+        console.log(`domcontentloaded`);
+    });
+    page.on('load', () => {
+        console.log(`load`);
+    });
+
+    await page.goto('https://www.baidu.com');
+})();
