d6f001283468b81beb3635068b78e9f2bbb0e942
uxei zmsy <xxx@gmail.com>
2025-02-26 11:41:56 +0800

    [UPDATE]

---
+++ lang-demo/javascript-demo/tencent-demo/face_id/id_card.js
@@ -0,0 +1,44 @@
+(async () => {
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let secretId = await new Promise(resolve => {
+        rl.question("input secretId: ", (answer) => {
+            resolve(answer);
+            rl.close();
+        });
+    });
+
+    let secretKey = await new Promise(resolve => {
+        rl.question("input secretKey: ", (answer) => {
+            resolve(answer);
+            rl.close();
+        });
+    });
+
+    const tencentcloud = require("tencentcloud-sdk-nodejs-faceid");
+    const client = new tencentcloud.faceid.v20180301.Client({
+        credential: {
+            // https://console.cloud.tencent.com/cam/capi
+            secretId: secretId,
+            secretKey: secretKey,
+        },
+        region: "",
+        profile: {
+            httpProfile: {
+                endpoint: "faceid.tencentcloudapi.com",
+            },
+        },
+    });
+    client.IdCardVerification({}).then(
+        data => {
+            console.log(data);
+        },
+        error => {
+            console.error(error);
+        }
+    );
+})();
