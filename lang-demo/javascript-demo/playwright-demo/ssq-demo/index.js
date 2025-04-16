6eb9e55a943fe958abb8b39f3d9f25c7b9541b5c
uxei zmsy <xxx@gmail.com>
2025-04-16 15:16:32 +0800

    [UPDATE]

--- lang-demo/javascript-demo/playwright-demo/ssq-demo/index.js
+++ lang-demo/javascript-demo/playwright-demo/ssq-demo/index.js
@@ -1,1 +1,54 @@
-console.log('Happy developing ✨')
+// noinspection JSUnresolvedReference,JSUnusedGlobalSymbols
+
+(async () => {
+    const {chromium} = require("playwright");
+    const browser = await chromium.launch({headless: false});
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto("https://www.zhcw.com/kjxx/ssq/");
+    await page.waitForLoadState("load", {timeout: 18000});
+    await page.waitForTimeout(3000);
+
+    const list = [];
+    let pages = 1;
+    let pageNum = 1;
+    while (pageNum <= pages) {
+        const resJson = await page.evaluate(pageNum => {
+            return new Promise(resolve => {
+                $.ajax({
+                    url: "//jc.zhcw.com/port/client_json.php",
+                    data: {
+                        "transactionType": "10001001",
+                        "lotteryId": "1",
+                        "issueCount": 0,
+                        "startIssue": 0,
+                        "endIssue": 0,
+                        "startDate": "2010-01-01",
+                        "endDate": "2026-01-01",
+                        "type": 2,
+                        "pageNum": pageNum,
+                        "pageSize": 500,
+                        "tt": Math.random()
+                    },
+                    dataType: "jsonp",
+                    jsonp: "callback",
+                    async: false,
+                    success: function (res) {
+                        if (res.resCode === "000000") {
+                            resolve(res);
+                        }
+                    }
+                });
+            });
+        }, pageNum);
+
+        list.push(...resJson.data);
+        pages = parseInt(resJson.pages);
+        pageNum++;
+    }
+
+    console.log(list);
+    await page.waitForTimeout(999999);
+    await browser.close();
+})();
