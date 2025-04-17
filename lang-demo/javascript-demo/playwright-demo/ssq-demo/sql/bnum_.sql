6af4b37525ddd97a72aa6039a8cdac57d2c3f056
uxei zmsy <xxx@gmail.com>
2025-04-17 15:39:50 +0800

    [UPDATE]

---
+++ lang-demo/javascript-demo/playwright-demo/ssq-demo/sql/bnum_.sql
@@ -0,0 +1,31 @@
+select '01' num, count(1) cnt from t_ssq where b = '01'
+union all
+select '02' num, count(1) cnt from t_ssq where b = '02'
+union all
+select '03' num, count(1) cnt from t_ssq where b = '03'
+union all
+select '04' num, count(1) cnt from t_ssq where b = '04'
+union all
+select '05' num, count(1) cnt from t_ssq where b = '05'
+union all
+select '06' num, count(1) cnt from t_ssq where b = '06'
+union all
+select '07' num, count(1) cnt from t_ssq where b = '07'
+union all
+select '08' num, count(1) cnt from t_ssq where b = '08'
+union all
+select '09' num, count(1) cnt from t_ssq where b = '09'
+union all
+select '10' num, count(1) cnt from t_ssq where b = '10'
+union all
+select '11' num, count(1) cnt from t_ssq where b = '11'
+union all
+select '12' num, count(1) cnt from t_ssq where b = '12'
+union all
+select '13' num, count(1) cnt from t_ssq where b = '13'
+union all
+select '14' num, count(1) cnt from t_ssq where b = '14'
+union all
+select '15' num, count(1) cnt from t_ssq where b = '15'
+union all
+select '16' num, count(1) cnt from t_ssq where b = '16';
