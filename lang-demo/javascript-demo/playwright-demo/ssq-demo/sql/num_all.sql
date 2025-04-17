a040b22da7b7d5417edf355f402b8a5863b929ca
uxei zmsy <xxx@gmail.com>
2025-04-17 14:38:22 +0800

    [UPDATE]

---
+++ lang-demo/javascript-demo/playwright-demo/ssq-demo/sql/num_all.sql
@@ -0,0 +1,65 @@
+select '01' as num, count(1) as cnt from t_ssq where r = '01' or r2 = '01' or r3 = '01' or r4 = '01' or r5 = '01' or r6 = '01'
+union all
+select '02', count(1) from t_ssq where r = '02' or r2 = '02' or r3 = '02' or r4 = '02' or r5 = '02' or r6 = '02'
+union all
+select '03', count(1) from t_ssq where r = '03' or r2 = '03' or r3 = '03' or r4 = '03' or r5 = '03' or r6 = '03'
+union all
+select '04', count(1) from t_ssq where r = '04' or r2 = '04' or r3 = '04' or r4 = '04' or r5 = '04' or r6 = '04'
+union all
+select '05', count(1) from t_ssq where r = '05' or r2 = '05' or r3 = '05' or r4 = '05' or r5 = '05' or r6 = '05'
+union all
+select '06', count(1) from t_ssq where r = '06' or r2 = '06' or r3 = '06' or r4 = '06' or r5 = '06' or r6 = '06'
+union all
+select '07', count(1) from t_ssq where r = '07' or r2 = '07' or r3 = '07' or r4 = '07' or r5 = '07' or r6 = '07'
+union all
+select '08', count(1) from t_ssq where r = '08' or r2 = '08' or r3 = '08' or r4 = '08' or r5 = '08' or r6 = '08'
+union all
+select '09', count(1) from t_ssq where r = '09' or r2 = '09' or r3 = '09' or r4 = '09' or r5 = '09' or r6 = '09'
+union all
+select '10', count(1) from t_ssq where r = '10' or r2 = '10' or r3 = '10' or r4 = '10' or r5 = '10' or r6 = '10'
+union all
+select '11', count(1) from t_ssq where r = '11' or r2 = '11' or r3 = '11' or r4 = '11' or r5 = '11' or r6 = '11'
+union all
+select '12', count(1) from t_ssq where r = '12' or r2 = '12' or r3 = '12' or r4 = '12' or r5 = '12' or r6 = '12'
+union all
+select '13', count(1) from t_ssq where r = '13' or r2 = '13' or r3 = '13' or r4 = '13' or r5 = '13' or r6 = '13'
+union all
+select '14', count(1) from t_ssq where r = '14' or r2 = '14' or r3 = '14' or r4 = '14' or r5 = '14' or r6 = '14'
+union all
+select '15', count(1) from t_ssq where r = '15' or r2 = '15' or r3 = '15' or r4 = '15' or r5 = '15' or r6 = '15'
+union all
+select '16', count(1) from t_ssq where r = '16' or r2 = '16' or r3 = '16' or r4 = '16' or r5 = '16' or r6 = '16'
+union all
+select '17', count(1) from t_ssq where r = '17' or r2 = '17' or r3 = '17' or r4 = '17' or r5 = '17' or r6 = '17'
+union all
+select '18', count(1) from t_ssq where r = '18' or r2 = '18' or r3 = '18' or r4 = '18' or r5 = '18' or r6 = '18'
+union all
+select '19', count(1) from t_ssq where r = '19' or r2 = '19' or r3 = '19' or r4 = '19' or r5 = '19' or r6 = '19'
+union all
+select '20', count(1) from t_ssq where r = '20' or r2 = '20' or r3 = '20' or r4 = '20' or r5 = '20' or r6 = '20'
+union all
+select '21', count(1) from t_ssq where r = '21' or r2 = '21' or r3 = '21' or r4 = '21' or r5 = '21' or r6 = '21'
+union all
+select '22', count(1) from t_ssq where r = '22' or r2 = '22' or r3 = '22' or r4 = '22' or r5 = '22' or r6 = '22'
+union all
+select '23', count(1) from t_ssq where r = '23' or r2 = '23' or r3 = '23' or r4 = '23' or r5 = '23' or r6 = '23'
+union all
+select '24', count(1) from t_ssq where r = '24' or r2 = '24' or r3 = '24' or r4 = '24' or r5 = '24' or r6 = '24'
+union all
+select '25', count(1) from t_ssq where r = '25' or r2 = '25' or r3 = '25' or r4 = '25' or r5 = '25' or r6 = '25'
+union all
+select '26', count(1) from t_ssq where r = '26' or r2 = '26' or r3 = '26' or r4 = '26' or r5 = '26' or r6 = '26'
+union all
+select '27', count(1) from t_ssq where r = '27' or r2 = '27' or r3 = '27' or r4 = '27' or r5 = '27' or r6 = '27'
+union all
+select '28', count(1) from t_ssq where r = '28' or r2 = '28' or r3 = '28' or r4 = '28' or r5 = '28' or r6 = '28'
+union all
+select '29', count(1) from t_ssq where r = '29' or r2 = '29' or r3 = '29' or r4 = '29' or r5 = '29' or r6 = '29'
+union all
+select '30', count(1) from t_ssq where r = '30' or r2 = '30' or r3 = '30' or r4 = '30' or r5 = '30' or r6 = '30'
+union all
+select '31', count(1) from t_ssq where r = '31' or r2 = '31' or r3 = '31' or r4 = '31' or r5 = '31' or r6 = '31'
+union all
+select '32', count(1) from t_ssq where r = '32' or r2 = '32' or r3 = '32' or r4 = '32' or r5 = '32' or r6 = '32'
+union all
+select '33', count(1) from t_ssq where r = '33' or r2 = '33' or r3 = '33' or r4 = '33' or r5 = '33' or r6 = '33';
