6d66718b81046f7df43e00685fcb0676267ed136
uxei zmsy <xxx@gmail.com>
2025-04-17 14:39:50 +0800

    [UPDATE]

---
+++ lang-demo/javascript-demo/playwright-demo/ssq-demo/sql/num_r.sql
@@ -0,0 +1,65 @@
+select '01' num, count(1) cnt from t_ssq where r = '01'
+union all
+select '02' num, count(1) cnt from t_ssq where r = '02'
+union all
+select '03' num, count(1) cnt from t_ssq where r = '03'
+union all
+select '04' num, count(1) cnt from t_ssq where r = '04'
+union all
+select '05' num, count(1) cnt from t_ssq where r = '05'
+union all
+select '06' num, count(1) cnt from t_ssq where r = '06'
+union all
+select '07' num, count(1) cnt from t_ssq where r = '07'
+union all
+select '08' num, count(1) cnt from t_ssq where r = '08'
+union all
+select '09' num, count(1) cnt from t_ssq where r = '09'
+union all
+select '10' num, count(1) cnt from t_ssq where r = '10'
+union all
+select '11' num, count(1) cnt from t_ssq where r = '11'
+union all
+select '12' num, count(1) cnt from t_ssq where r = '12'
+union all
+select '13' num, count(1) cnt from t_ssq where r = '13'
+union all
+select '14' num, count(1) cnt from t_ssq where r = '14'
+union all
+select '15' num, count(1) cnt from t_ssq where r = '15'
+union all
+select '16' num, count(1) cnt from t_ssq where r = '16'
+union all
+select '17' num, count(1) cnt from t_ssq where r = '17'
+union all
+select '18' num, count(1) cnt from t_ssq where r = '18'
+union all
+select '19' num, count(1) cnt from t_ssq where r = '19'
+union all
+select '20' num, count(1) cnt from t_ssq where r = '20'
+union all
+select '21' num, count(1) cnt from t_ssq where r = '21'
+union all
+select '22' num, count(1) cnt from t_ssq where r = '22'
+union all
+select '23' num, count(1) cnt from t_ssq where r = '23'
+union all
+select '24' num, count(1) cnt from t_ssq where r = '24'
+union all
+select '25' num, count(1) cnt from t_ssq where r = '25'
+union all
+select '26' num, count(1) cnt from t_ssq where r = '26'
+union all
+select '27' num, count(1) cnt from t_ssq where r = '27'
+union all
+select '28' num, count(1) cnt from t_ssq where r = '28'
+union all
+select '29' num, count(1) cnt from t_ssq where r = '29'
+union all
+select '30' num, count(1) cnt from t_ssq where r = '30'
+union all
+select '31' num, count(1) cnt from t_ssq where r = '31'
+union all
+select '32' num, count(1) cnt from t_ssq where r = '32'
+union all
+select '33' num, count(1) cnt from t_ssq where r = '33';
