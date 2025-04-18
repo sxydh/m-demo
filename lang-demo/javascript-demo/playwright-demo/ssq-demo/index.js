3cc14389f0fe97abf47899d6a2f0311821ed542f
uxei zmsy <xxx@gmail.com>
2025-04-18 11:15:33 +0800

    [UPDATE]

--- lang-demo/javascript-demo/playwright-demo/ssq-demo/index.js
+++ lang-demo/javascript-demo/playwright-demo/ssq-demo/index.js
@@ -49,6 +49,7 @@ const initData = async () => {
     }
 
     const db = require("better-sqlite3")("index.db", {});
+    db.pragma('journal_mode = WAL');
     db.exec("DROP TABLE t_ssq");
     db.exec("CREATE TABLE IF NOT EXISTS t_ssq (id INTEGER PRIMARY KEY AUTOINCREMENT, d TEXT, r TEXT, r2 TEXT, r3 TEXT, r4 TEXT, r5 TEXT, r6 TEXT, b TEXT)");
 
