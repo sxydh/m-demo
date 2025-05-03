83dd713cef4bba95e98b24de2620a35d7b55847d
uxei zmsy <xxx@gmail.com>
2025-05-03 19:54:36 +0800

    [UPDATE]

---
+++ lang-demo/python-demo/pynput-demo/mouse_click.py
@@ -0,0 +1,9 @@
+from pynput import mouse
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        print(f"鼠标点击位置: ({x}, {y})")
+
+# 监听鼠标事件
+with mouse.Listener(on_click=on_click) as listener:
+    listener.join()
