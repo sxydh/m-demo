fd5cc7db5cfaa1d34ac36138c8a83ee0cd0e9f61
sxydh <sxydhgg@gmail.com>
2025-06-17 15:31:07 +0800

    [UPDATE]

--- lang-demo/python-demo/cryptography-demo/src/main/_demo.py
+++ lang-demo/python-demo/cryptography-demo/src/main/_demo.py
@@ -1,6 +1,6 @@
 from base64 import urlsafe_b64encode
 
-from cryptography.fernet import Fernet, InvalidToken
+from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
