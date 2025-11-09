4a8a6db8024ce7932d51059d3aefa9f790f90ce0
sxydh <sxydhgg@gmail.com>
2025-11-09 10:35:52 +0800

    [UPDATE]

--- lang-demo/python-demo/pytorch-demo/_demo.py
+++ lang-demo/python-demo/pytorch-demo/_demo.py
@@ -6,3 +6,10 @@ print(torch.__version__)
 print(torch.cuda.is_available())
 print(torchvision.__version__)
 print(torchaudio.__version__)
+
+# 输出示例：
+# 2.9.0+cu130
+# True
+# 0.24.0+cu130
+# 2.9.0+cu130
+# 如果输出不一致，考虑全局安装的组件是否影响。
