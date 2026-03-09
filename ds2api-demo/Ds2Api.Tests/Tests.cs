1c509694934e221d1b8198ac3255b41cbf554279
abc <abc@email.com>
2026-03-09 17:20:07 +0800

    feat: Ds2Api.Tests

---
+++ ds2api-demo/Ds2Api.Tests/Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace Ds2Api.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
